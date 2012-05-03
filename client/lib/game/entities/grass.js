0f76957b9b399830a9fd3a3e5da43c975c4ccadf
Jonathan Commins <joncom@gmail.com>
2012-05-02 21:55:49 -0700

    Instead of using timers and a hide() function, init() just sets zIndex once and provides similar effect

--- client/lib/game/entities/grass.js
+++ client/lib/game/entities/grass.js
@@ -9,12 +9,12 @@ ig.module(
 EntityGrass = ig.Entity.extend({
 	size: {x: 16, y: 16},
 	animSheet: new ig.AnimationSheet( 'media/grass-animation.png', 16, 16 ),
-	coverTimer: null, // timer used to hide player behind grass
-	hideTimer: null, // used to make this entity invis after player leaves
 	checkAgainst: ig.Entity.TYPE.A,
 	
 	init: function( x, y, settings ) {
 		this.parent( x, y, settings );
+		
+		this.zIndex = 3 + this.pos.y, // above player, otherplayer, and npc
 		
 		this.addAnim( 'rustle', 0.1667, [0,1,2,3,4], true ); // 10 frames of 60 per
 		this.currentAnim = null; // invisible by default
@@ -24,30 +24,10 @@ EntityGrass = ig.Entity.extend({
 	{
 		this.currentAnim = this.anims.rustle;
 		this.currentAnim.rewind();
-		this.aboveTimer = new ig.Timer();
-		this.aboveTimer.set(0.183) // 11/60th of a sec
-		
-		// after 11/60th of a sec place entity above player
-	},
-	
-	hide: function()
-	{
-		console.debug("Hiding patch of grass");
-		//this.currentAnim = null;
-		this.zIndex = this.pos.y + 0; // below player all players
-		ig.game.sortEntitiesDeferred();
 	},
 	
 	update: function()
 	{
-		if(this.aboveTimer != null && this.aboveTimer.delta() >= 0)
-		{
-			console.debug("Putting patch of grass above player");
-			this.zIndex = 3 + this.pos.y; // above all players and npcs
-			ig.game.sortEntitiesDeferred();
-			delete this.aboveTimer;
-		}
-		
 		this.parent();
 	}
 });
