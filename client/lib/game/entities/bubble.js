b265d024812daf454f578450d9d282e10864fb7b
Jonathan Commins <joncom@gmail.com>
2012-05-07 18:19:56 -0700

    Loads graphics

--- client/lib/game/entities/bubble.js
+++ client/lib/game/entities/bubble.js
@@ -8,6 +8,13 @@ ig.module(
 
 EntityBubble = ig.Entity.extend({
 	animSheet: new ig.AnimationSheet( 'media/rs.jump.png', 16, 8 ),
+	
+	// load graphics
+	topLeft: new ig.Image( 'media/chat-bubble-tleft.png' ),
+	topRight: new ig.Image( 'media/chat-bubble-tright.png' ),
+	bottomLeft: new ig.Image( 'media/chat-bubble-bleft.png' ),
+	bottomRight: new ig.Image( 'media/chat-bubble-bright.png' ),
+	pointer: new ig.Image( 'media/chat-bubble-point.png' ),
 
 	init: function( x, y, settings ) {
 		this.parent( x, y, settings );
