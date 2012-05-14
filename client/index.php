78714b13eac9010695c137c3155b7e81588950a4
Jonathan Commins <joncom@gmail.com>
2012-05-14 14:14:06 -0700

    Changed local socket server address

--- client/index.php
+++ client/index.php
@@ -4,8 +4,8 @@
 	 <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.js" ></script>
 	 <script type="text/javascript" src="getUsernameFromURL.js" ></script>
 	 <?php
-		  if($_SERVER["SERVER_NAME"]=="192.168.1.95")
-			   echo "<script type=\"text/javascript\" src=\"http://192.168.1.95:8080/socket.io/socket.io.js\"></script>\n";
+		  if($_SERVER["SERVER_NAME"]=="192.168.1.103")
+			   echo "<script type=\"text/javascript\" src=\"http://192.168.1.103:8080/socket.io/socket.io.js\"></script>\n";
 		  else
 			   echo "<script type=\"text/javascript\" src=\"http://h.commins.ca:8080/socket.io/socket.io.js\"></script>\n";
 	 ?>
