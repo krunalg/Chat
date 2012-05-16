2d695d0688433c146dcd676adb7b0af6334d4a29
Jonathan Commins <joncom@gmail.com>
2012-05-15 22:07:47 -0700

    Now generates hashes from entire tilesheets

--- mapper/tilesheets.php
+++ mapper/tilesheets.php
@@ -67,35 +67,63 @@ function trim1px($file)
 
 
 
-
-
+/*
+ * variables
+ *
+ */ 
 $tilesize = 16;
-$path = "maps"; // dir to scan for tilesheets
-$tilesheets = scanFileNameRecursivly($path);
+$pathToMaps = "maps"; // dir to scan for tilesheets
 
-echo "<pre>";
-var_dump($tilesheets);
-echo "</pre>";
 
-// for each tilesheet found, trim 1px if nessesary
+
+/*
+ * create an array containing path of every tilesheet
+ *
+ */ 
+$tilesheets = scanFileNameRecursivly('.'.DIRECTORY_SEPARATOR.$pathToMaps);
+echo "<pre>"; var_dump($tilesheets); echo "</pre>";
+
+
+
+/*
+ * for each tilesheet, trim 1px if nessesary
+ *
+ */ 
 for($i=0; $i<count($tilesheets); $i++)
 {
-    $size = getimagesize($tilesheets[$i]);
+    $size  = getimagesize($tilesheets[$i]);
     $width = $size[0];
-    //die("if($width - 1 % $tilesize == 0)");
     if( ($width - 1) % $tilesize == 0) // if subtracting 1px is helpful
     {
-        echo "Needs trimming.";
+        echo "Needs trimming.<br>";
         trim1px($tilesheets[$i]);
     }
     else
-        echo "No trimming needed.";
+        echo "No trimming needed.<br>";
 }
 
 
-// make a function that trims off 1px
 
-
+/*
+ * generates hash values for each tilesheet
+ *
+ */ 
+for($i=0; $i<count($tilesheets); $i++)
+{
+    $size   = getimagesize($tilesheets[$i]);
+    $width  = $size[0];
+    $height = $size[1];
+    $hashes = "";
+    for($y=0; $y<$height/$tilesize; $y++)
+    {
+        for($x=0; $x<$width/$tilesize; $x++)
+        {
+            $img    = LoadPNG($tilesheets[$i]);
+            $hashes = $hashes . getTile($img, $tilesize, $x, $y);
+        }
+    }
+    echo "Hash for $tilesheets[$i] is: " . md5($hashes) ."<br>\n";
+}
 
 
 
