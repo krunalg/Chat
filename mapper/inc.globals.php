f750dfadb3ca11c8dfc3c32c637109d24a75bc14
Jonathan Commins <joncom@gmail.com>
2012-05-17 20:53:41 -0700

    Added collision graphics into global vars

--- mapper/inc.globals.php
+++ mapper/inc.globals.php
@@ -5,14 +5,30 @@ $globalMapDir = ".\maps";
 $globalCollisionsFile = 'collisions.txt';
 $globalTilesheetFile = 'tilesheet.png';
 
-// collision values in Weltmeister
-// for one-way hoppable ledges
+// collision values
+/*
 $collisionWalkable = 0;
 $collisionNoWalk = 1;
 $collisionLeft = 45;
 $collisionRight = 34;
 $collisionUp = 12;
 $collisionDown = 23;
+*/
+
+$collisionWalkableMouseoutImg = 'images/spacer.png';
+$collisionNoWalkMouseoutImg = 'images/solid.gif';
+$collisionLeftMouseoutImg = 'images/left.gif';
+$collisionRightMouseoutImg = 'images/right.gif';
+$collisionUpMouseoutImg = 'images/up.gif';
+$collisionDownMouseoutImg = 'images/down.gif';
+
+$collisionWalkableMouseoverImg = 'images/mouseover.png';
+$collisionNoWalkMouseoverImg = 'images/solid-mouseover.png';
+$collisionLeftMouseoverImg = 'images/left-mouseover.png';
+$collisionRightMouseoverImg = 'images/right-mouseover.png';
+$collisionUpMouseoverImg = 'images/up-mouseover.png';
+$collisionDownMouseoverImg = 'images/down-mouseover.png';
 
 
-?>
+
+?>     
