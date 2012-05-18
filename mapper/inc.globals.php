8c4aa77d3e0dde259e71dd6661fed227c4479cec
Jonathan Commins <joncom@gmail.com>
2012-05-17 23:53:39 -0700

    Added Weltmeister collisions vars to globals

--- mapper/inc.globals.php
+++ mapper/inc.globals.php
@@ -5,13 +5,25 @@ $globalMapDir = ".\maps";
 $globalCollisionsFile = 'collisions.txt';
 $globalTilesheetFile = 'tilesheet.png';
 
-// used later to map collision types to a Weltmeister collision value
+// used later to map collision types
+// to a Weltmeister collision value.
+// the _first value_ is the internal
+// collision value used by the code
+// generator. the _second value_ is
+// used at the very end when we
+// generate a map.
 $collisionWalkable = 0;
+$collisionWalkableWM = 0;
 $collisionNoWalk = 1;
+$collisionNoWalkWM = 1;
 $collisionLeft = 2;
+$collisionLeftWM = 45;
 $collisionRight = 3;
+$collisionRightWM = 34;
 $collisionUp = 4;
+$collisionUpWM = 12;
 $collisionDown = 5;
+$collisionDownWM = 23;
 
 $collisionWalkableMouseoutImg = 'images/spacer.png';
 $collisionNoWalkMouseoutImg = 'images/solid.gif';
