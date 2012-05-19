fbdfa49cee281ff93e9f1d5cd11424866f1c4442
Jonathan Commins <joncom@gmail.com>
2012-05-18 19:15:37 -0700

    So far master.php scans map folder, finds map files, and extracts directory from that

--- mapper/master.php
+++ mapper/master.php
@@ -7,9 +7,18 @@ include('inc.globals.php');
 require('required.php');
 
 // get a list of all maps
-$tilesheets = scanFileNameRecursivly($globalMapDir, $globalTilesheetFile);
+$maps = scanFileNameRecursivly($globalMapDir, $globalTilesheetFile);
 
-
+// check if the map has been processed yet or not
+for($i=0; $i<count($maps); $i++)
+{
+    $mapPath = explode(DIRECTORY_SEPARATOR, $maps[$i]);
+    $mapDirectory = '';
+    // remove just the filename, leaving us with files directory
+    for($j=0; $j < (count($mapPath) - 1) ; $j++)
+        $mapDirectory = $mapDirectory . $mapPath[$j] . DIRECTORY_SEPARATOR;
+    echo $mapDirectory . "<br>\n";
+}
 
 
 
