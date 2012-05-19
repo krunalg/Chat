9d50d701ea92fd3c581001ff40a34f0abda1d487
Jonathan Commins <joncom@gmail.com>
2012-05-18 23:13:50 -0700

    Started used-tiles.php, a script that reads all map JSON files and creates a single list of tiles used throughout the game world; Updated README

---
+++ mapper/used-tiles.php
@@ -0,0 +1,17 @@
+<?php
+
+set_time_limit(300);
+ini_set('memory_limit','512M'); // and use a lot of memory
+
+
+include('inc.globals.php');
+require('required.php');
+echo '<script type="text/javascript" src="inc.functions.js" ></script>'; // used for submitting forms
+
+
+
+
+
+
+
+?>
