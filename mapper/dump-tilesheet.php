b18e05cf23ea6257f9228e5d2a4db782f5bac068
Jonathan Commins <joncom@gmail.com>
2012-05-19 00:54:06 -0700

    Started dump-tilesheet.php; Added README info for it

---
+++ mapper/dump-tilesheet.php
@@ -0,0 +1,33 @@
+<?php
+
+set_time_limit(300);
+ini_set('memory_limit','512M'); // and use a lot of memory
+
+
+include('inc.globals.php');
+require('required.php');
+echo '<script type="text/javascript" src="inc.functions.js" ></script>'; // used for submitting forms
+
+
+if( !isset($_POST['dump']) )
+{
+    /*
+     * First Page: 
+     *
+     */
+    
+}
+else if(isset($_POST['dump']))
+{
+    /*
+     * Second Page: 
+     *
+     */
+    
+    
+}
+
+
+
+
+?>
