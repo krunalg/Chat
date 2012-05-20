2e640276e41cdb1562935c1c380e03cb00b11e79
Jonathan Commins <joncom@gmail.com>
2012-05-20 12:38:42 -0700

    Modified to edit collisions directly on maps, rather than tilesheets

--- mapper/collisions.php
+++ mapper/collisions.php
@@ -17,25 +17,25 @@ require('inc.functions.php');
 if( !isset($_GET['ts']) && !isset($_POST['tiles']) )
 { 
     /*
-     * First Page: Select a tilesheet
+     * First Page: Select a map
      *
      */
     
-    // get a list of all tilesheets
-    $tilesheets = scanFileNameRecursivly($globalMapDir, $globalTilesheetFile);
+    // get a list of all maps
+    $maps = scanFileNameRecursivly($globalMapDir, $globalMapFilename);
     
     // start html form
     echo '<form method="get">';
     
     // echo html form element
     echo '<select name="ts" multiple="multiple">' . "\n";
-        for($i=0; $i<count($tilesheets); $i++)
+        for($i=0; $i<count($maps); $i++)
         {
-            $explode = explode('\\', $tilesheets[$i]);
+            $explode = explode('\\', $maps[$i]);
             $fileName = $explode[count($explode)-1];
             $dirName = $explode[count($explode)-2];
             if($i==0) $selected = 'selected="selected" '; else $selected = '';
-            echo '<option '.$selected.'value="'.$tilesheets[$i].'">'.$dirName.'</option>' . "\n";
+            echo '<option '.$selected.'value="'.$maps[$i].'">'.$dirName.'</option>' . "\n";
         }
         echo '</select>';
     
@@ -49,7 +49,7 @@ if( !isset($_GET['ts']) && !isset($_POST['tiles']) )
 else if( isset($_GET['ts']) )
 {
     /*
-     * Second Page: Display tilesheet
+     * Second Page: Display map
      *
      */
     
@@ -64,13 +64,13 @@ else if( isset($_GET['ts']) )
     $ts = $_GET['ts'];
     
     $size = getimagesize($ts);
-    $width = $size[0]; // width in px of tilesheet
-    $height = $size[1]; // height in px of tilesheet
+    $width = $size[0]; // width in px of map
+    $height = $size[1]; // height in px of map
     $widthInTiles = $width/$globalTilesize;
     $heightInTiles = $height/$globalTilesize;
     
-    // load tilesheet for grabbing hashes
-    $tilesheet = LoadPNG($ts);
+    // load map for grabbing hashes
+    $map = LoadPNG($ts);
     
     // comment slashes so background image loads
     $ts = str_replace('\\', "\\\\", $ts);
@@ -84,7 +84,7 @@ else if( isset($_GET['ts']) )
         echo 'tiles['.$x.'] = new Object();'."\n";
         for($y=0; $y<$heightInTiles; $y++)
         {
-            $hash = getTile($tilesheet, $globalTilesize, $x, $y);
+            $hash = getTile($map, $globalTilesize, $x, $y);
             $collision = 0;
             if(isset($collisions[$hash])) $collision = $collisions[$hash];
             echo 'tiles['.$x.']['.$y.'] = new Object();'."\n";
@@ -95,7 +95,7 @@ else if( isset($_GET['ts']) )
     echo '</script>' ."\n";
     echo '<!-- ending tile hash values -->' ."\n\n";
     
-    // create main tilesheet div w/ bg
+    // create main map div w/ bg
     echo '<div style="'.
         'background: url(\''.$ts.'\'); '.
         'position: absolute; '.
@@ -140,7 +140,7 @@ else if( isset($_GET['ts']) )
         }
     
     
-    echo '</div>'; // close main tilesheet div
+    echo '</div>'; // close main map div
     
     
 }
@@ -163,6 +163,7 @@ else if( isset($_POST['tiles']) )
     // update collisions array
     for($i=0; $i<count($newCollisions); $i++)
     {
+        // use hash for index so no duplicates
         $collisions[ $newCollisions[$i][0] ] = $newCollisions[$i][1];
     }
     
@@ -179,7 +180,7 @@ else if( isset($_POST['tiles']) )
             echo "Success writing file: " . $globalCollisionsFile;
     }
     else echo "Nothing to write to file.";
-    echo '<br /><a href="">Edit another tilesheet</a>';
+    echo '<br /><a href="">Edit another map</a>';
     
     //echo "I found the following data: \n" . $_POST['tiles'];
 }
@@ -227,9 +228,10 @@ else
         {
             for(var y in tiles[x])
             {
-                if(tiles[x][y].collision!=0)
+                //if(tiles[x][y].collision!=0)
+                    // dump even the walkable tiles because we need to be able to convert tiles to walkable
                     dump = dump + tiles[x][y].hash + ":" + collisionTypes[tiles[x][y].collision] + "\n";
-                else
+                //else
                     ;//dump = dump + "Added the following dummy data instead: " + tiles[i][j].collision + "\n";
             }
         }
@@ -243,6 +245,21 @@ else
             tiles[x][y].collision = 0; // restart cycle if need be
         
         tileOver(x,y); // update displayed image
+        
+        // update others with same hash
+        var othersCollision = tiles[x][y].collision;
+        var othersHash = tiles[x][y].hash;
+        for(var othersX in tiles)
+        {
+            for(var othersY in tiles[x])
+            {
+                if(tiles[othersX][othersY].hash==othersHash)
+                {
+                    tiles[othersX][othersY].collision = othersCollision;
+                    tileOut(othersX, othersY);
+                }
+            }
+        }
     }
     
     var tileOver = function(x, y)
