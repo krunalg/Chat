6117e74f2a759315a9a8f2393bbcd1d30f911ddd
Jonathan Commins <joncom@gmail.com>
2012-05-29 15:11:39 -0700

    First commit of background-animations.js, which I will not be tracking because it contains generated code, at least not right now

---
+++ client/lib/game/background-animations.js
@@ -0,0 +1,9 @@
+ig.module('game.background-animations')
+.requires(
+    'impact.game'
+)
+.defines(function() {
+    initBackgroundAnimations = function() {
+        // animation code from animations.php goes here...
+    }
+})
