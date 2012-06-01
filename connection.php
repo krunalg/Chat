6f46db171a2628506185b7c78bb1c4b3a5d1335d
Jonathan Commins <joncom@gmail.com>
2012-05-31 20:00:05 -0700

    Added single file in which to store socket server IP address

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_SERVER["SERVER_NAME"]!="commins.ca")
+{
+  $socketHost = '192.168.1.74';
+  $socketPort = 9090;
+}
+else
+{
+  $socketHost = 'h.commins.ca';
+  $socketPort = 9090;
+}
+
+
+?>
