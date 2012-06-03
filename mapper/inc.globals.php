3c90d85a79666a172a768acbc20172ed4fb4d436
Jonathan Commins <joncom@gmail.com>
2012-06-02 19:56:34 -0700

    Added WeltmeisterTileOffset global var.

--- mapper/inc.globals.php
+++ mapper/inc.globals.php
@@ -5,6 +5,7 @@ $globalTilesize = 16;
 $globalMasterTilesheetWidth = 256;
 $globalBorderRepeatX = 0; // 4;
 $globalBorderRepeatY = 0; // 3;
+$globalWMTileOffset = 1; // because Weltmeister considers 1, not 0 to be the first tile
 
 // DIRECTORIES
 $globalMapDir =
