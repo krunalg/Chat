bf0008e4a6aeb837d9a111cc7bc48e653698f29d
Jonathan Commins <joncom@gmail.com>
2012-06-04 19:04:48 -0700

    Performed jsFormat; removed unused properties; Removed call to parent() from draw(); Added comments.

--- client/lib/game/entities/non-weltmeister/name.js
+++ client/lib/game/entities/non-weltmeister/name.js
@@ -1,78 +1,81 @@
-ig.module(
-	'game.entities.non-weltmeister.name'
-)
-.requires(
-	'impact.entity',
-	'impact.font'
-)
-.defines(function(){
+ig.module('game.entities.non-weltmeister.name').requires('impact.entity', 'impact.font').defines(function() {
 
-EntityName = ig.Entity.extend({
-	
-	white: new ig.Font( 'media/font.white.with.shadow.png' ),
-	blue: new ig.Font( 'media/font.blue.with.shadow.png' ),
-	green: new ig.Font( 'media/font.green.with.shadow.png' ),
-	
-	size: { x: 16, y: 16 },
-	color: null, // what color font to use
-	follow: null, // name of entity to follow
-	hideTimer: null, // used to temporarily not draw
-	
-	init: function( x, y, settings ) {
-		this.parent( x, y, settings );
-		
-		this.hideTimer = new ig.Timer();
-	},	
-	
-	draw: function( reallyDraw )
-	{
-		// Only draw when the 'reallyDraw' param is true, 
-		// so it ignores the "normal" draw call.
-		// Additionally, draw only if we shouldn't hide instead
-		if( reallyDraw && this.hideTimer.delta() >= 0 )
-		{
-			var target = ig.game.getEntityByName(this.follow);
-			if(target!=undefined)
-			{
-				this.pos.x = target.pos.x;
-				this.pos.y = target.pos.y;
-			}
-			else
-			{
-				console.debug( "Name entity could not find entity '" +
-					       this.follow + "' and will now kill() itself.");
-				this.kill();
-			}
-			
-			switch(this.color)
-			{
+	EntityName = ig.Entity.extend({
+
+		white: new ig.Font('media/font.white.with.shadow.png'),
+		blue: new ig.Font('media/font.blue.with.shadow.png'),
+		green: new ig.Font('media/font.green.with.shadow.png'),
+
+		size: {
+			x: 16,
+			y: 16
+		},
+
+		// what color font to use
+		color: null,
+
+		// name of entity to follow
+		follow: null,
+
+		// used to temporarily not draw
+		hideTimer: new ig.Timer(),
+
+		init: function(x, y, settings) {
+			this.parent(x, y, settings);
+		},
+
+		draw: function(reallyDraw) {
+			// Only draw when the 'reallyDraw' param is true, 
+			// so it ignores the "normal" draw call.
+			// Additionally, draw only if we shouldn't hide instead
+			if (reallyDraw && this.hideTimer.delta() >= 0) {
+
+				// Create reference to player.
+				var player = ig.game.getEntityByName(this.follow);
+
+				if (player != undefined) {
+
+					// Align to players position.
+					this.pos.x = player.pos.x;
+					this.pos.y = player.pos.y;
+
+					// Set name so this entity can be found.
+					this.name = player.name + "-name";
+
+				} else {
+
+					// Write debug message.
+					console.debug("EntityName does not an Entity by the name '" + this.follow + "' and will now kill() itself.");
+
+					// Free up resources.
+					this.kill();
+
+				}
+
+				// Select font color.
+				switch (this.color) {
 				case 'green':
 				case 'blue':
 				case 'white':
-					this[this.color].draw(
-						this.follow,
-						this.pos.x - ig.game.screen.x + this.size.x/2,
-						this.pos.y - ig.game.screen.y - this.size.y,
-						ig.Font.ALIGN.CENTER
-					);
-					break;
-				default:
-					console.debug( "Name entity was not supplied valid " +
-						       "color and will now kill() itself.");
-					this.kill();
-					break;
-			}
-			
-			
-			this.parent();
-		}
-	},
-	
-	update: function()
-	{
-		this.parent();
-	}
-	
-});
 
+					// Draw font to screen.
+					this[this.color].draw(
+					this.follow, this.pos.x - ig.game.screen.x + this.size.x / 2, this.pos.y - ig.game.screen.y - this.size.y, ig.Font.ALIGN.CENTER);
+					break;
+
+				default:
+
+					console.debug("EntityName was not supplied valid color and will now kill() itself.");
+					
+					// Free up resources.
+					this.kill();
+					
+					break;
+
+				}
+				//this.parent();
+			}
+		}
+
+	});
 });
