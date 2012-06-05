988f831ef483075c719b55dfa93d54325dee7431
Jonathan Commins <joncom@gmail.com>
2012-06-05 15:43:48 -0700

    Performed jsFormat on surf.js

--- client/lib/game/entities/non-weltmeister/surf.js
+++ client/lib/game/entities/non-weltmeister/surf.js
@@ -1,85 +1,82 @@
-ig.module(
-	'game.entities.non-weltmeister.surf'
-)
-.requires(
-	'impact.entity'
-)
-.defines(function(){
+ig.module('game.entities.non-weltmeister.surf')
 
-EntitySurf = ig.Entity.extend({
-	
-	size: { x: 16, y: 16 },
-	offset: { x: 8, y: 8 },
+.requires('impact.entity')
 
-	follow: null, // name of entity to follow
+.defines(function() {
 
-	animSheet: new ig.AnimationSheet( 'media/rs.surf.png', 32, 32 ),
-	
-	init: function( x, y, settings ) {
-		this.parent( x, y, settings );
+	EntitySurf = ig.Entity.extend({
 
-		// Add each possible faced state.
-		this.addAnim('up', 1, [0], true);
-		this.addAnim('down', 1, [2], true);
-		this.addAnim('left', 1, [1], true);
-		this.addAnim('right', 1, [1], true);
-		
-		// Flip the image for facing right.
-		this.anims.right.flip.x = true;
+		size: {
+			x: 16,
+			y: 16
+		},
+		offset: {
+			x: 8,
+			y: 8
+		},
 
-		// Set current animation.
-		this.resetAnimation();
-	},
+		// name of entity to follow
+		follow: null,
 
-	resetAnimation: function()
-	{
-		switch(this.facing) {
+		animSheet: new ig.AnimationSheet('media/rs.surf.png', 32, 32),
+
+		init: function(x, y, settings) {
+			this.parent(x, y, settings);
+
+			// Add each possible faced state.
+			this.addAnim('up', 1, [0], true);
+			this.addAnim('down', 1, [2], true);
+			this.addAnim('left', 1, [1], true);
+			this.addAnim('right', 1, [1], true);
+
+			// Flip the image for facing right.
+			this.anims.right.flip.x = true;
+
+			// Set current animation.
+			this.resetAnimation();
+		},
+
+		resetAnimation: function() {
+			switch (this.facing) {
 			case 'left':
 			case 'right':
 			case 'up':
 			case 'down':
 				this.currentAnim = this.anims[this.facing]
 				break;
-			default: 
+			default:
 				throw "Error: tried giving surf entity illegal direction to face.";
 				break;
-		}
-	},
-	
-	draw: function()
-	{
-		var player = ig.game.getEntityByName(this.follow);
-		if(player!=undefined)
-		{
-			// Kill entity when no long swimming.
-			if(!player.swimming) this.kill();
-			
-			// Update direction according to player.
-			if(this.facing!=player.facing) 
-			{
-				this.facing = player.facing;
-				this.resetAnimation();
 			}
-			
-			// Copy coordinates of player.
-			this.pos.x = player.pos.x;
-			this.pos.y = player.pos.y;
-		}
-		else
-		{
-			console.debug( "Surf entity could not find entity '" +
-				this.follow + "' and will now kill() itself.");
-			this.kill();
-		}
-		
-		this.parent();
-	},
-	
-	update: function()
-	{
-		this.parent();
-	}
-	
-});
+		},
 
+		draw: function() {
+			var player = ig.game.getEntityByName(this.follow);
+			if (player != undefined) {
+				// Kill entity when no long swimming.
+				if (!player.swimming) this.kill();
+
+				// Update direction according to player.
+				if (this.facing != player.facing) {
+					this.facing = player.facing;
+					this.resetAnimation();
+				}
+
+				// Copy coordinates of player.
+				this.pos.x = player.pos.x;
+				this.pos.y = player.pos.y;
+			} else {
+				console.debug("Surf entity could not find entity '" + this.follow + "' and will now kill() itself.");
+				this.kill();
+			}
+
+			this.parent();
+		},
+
+		update: function() {
+			this.parent();
+		}
+
+
+	});
 });
