56288b94599845b0a64bcfd157b9f8a8c1c3baa5
Jonathan Commins <joncom@gmail.com>
2012-06-05 19:40:42 -0700

    Grass entity now uses xPriority property instead of magic number for zIndex; Added many comments.

--- client/lib/game/entities/grass.js
+++ client/lib/game/entities/grass.js
@@ -7,13 +7,25 @@ ig.module('game.entities.grass').requires('impact.entity').defines(function() {
 			y: 16
 		},
 
+		// Should be above player entities zPriority.
+		zPriority: 4,
+
+		// Load image resource.
 		animSheet: new ig.AnimationSheet('media/grass-animation.png', 16, 16),
+		
+		// Used to keep entity alive briefly before killing.
 		killTimer: null,
+
+		// Is set to true when markForDeath() is called.
 		markedForDeath: false,
 
 		// Start count-down until this entity will be killed.
 		markForDeath: function() {
+			
+			// Set the timer.
 			this.killTimer.set(3);
+
+			// Allow removal of entity.
 			this.markedForDeath = true;
 		},
 
@@ -22,20 +34,25 @@ ig.module('game.entities.grass').requires('impact.entity').defines(function() {
 			this.markedForDeath = false;
 		},
 
-		// Initiate
 		init: function(x, y, settings) {
 			this.parent(x, y, settings);
 
 			// Create timer to count-down to entity's death.
 			this.killTimer = new ig.Timer();
 
-			this.zIndex = 4 + this.pos.y, // above players
+			// Set zIndex.
+			this.zIndex = this.zPriority + this.pos.y,
+			
+			// Create animation.
 			this.addAnim('rustle', 0.1667, [0, 1, 2, 3, 4], true); // 10 frames of 60 per
-			this.currentAnim = null; // invisible by default
+			
+			// No animation until one is triggered.
+			this.currentAnim = null;
 		},
 
 		play: function() {
-			// Reset animation to first frame.
+			
+			// Rewind to first frame.
 			this.anims.rustle.rewind();
 
 			// Set animation.
