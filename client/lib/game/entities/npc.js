24b3159a44bd7a151e7ec71da234286d3fcb8de4
Jonathan Commins <joncom@gmail.com>
2012-06-06 21:38:37 -0700

    Added comments.

--- client/lib/game/entities/npc.js
+++ client/lib/game/entities/npc.js
@@ -5,29 +5,46 @@ ig.module(
 .requires(
 
 'game.entities.non-weltmeister.player').defines(function() {
+
 	EntityNpc = EntityPlayer.extend({
 
+		// Layering priority relative to other entities.
 		zPriority: 2,
 
-		// NPC movement patterns
+		// An array of directions an NPC will move.
 		movePattern: [],
 
-		// no pattern by default
+		// The next move in the pattern to perform.
 		moveNext: 0,
 
+		// Used to know when next move occurs.
 		moveTimer: null,
 
+		// Time in seconds between moves.
 		moveDelay: 2,
 
-		// delay in seconds between moves
+		// Changes the players faced direction.
 		faceNextMove: function() {
+			
+			// Face direction of next move in the pattern.
 			this.facing = this.movePattern[this.moveNext];
+
+			// Update graphic.
 			this.moveAnimStop();
 		},
 
+		// Sets the next move in the pattern.
 		justMoved: function() {
+			
+			// Select next move in pattern.
 			this.moveNext++;
-			if (this.moveNext >= this.movePattern.length) this.moveNext = 0; // cycle pattern
+
+			// Check if we reached the end of the pattern.
+			if (this.moveNext >= this.movePattern.length) 
+			{
+				// Start the pattern over again.
+				this.moveNext = 0;
+			}
 		},
 
 		startMove: function() {
