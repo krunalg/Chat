dc2d7b77878540887df1b8db754a382a2e931844
Jonathan Commins <joncom@gmail.com>
2012-06-09 19:20:45 -0700

    Performed jsFormat on reflection.js

--- client/lib/game/entities/non-weltmeister/reflection.js
+++ client/lib/game/entities/non-weltmeister/reflection.js
@@ -31,41 +31,39 @@ ig.module('game.entities.non-weltmeister.reflection')
 
 			// Adds the flipX and flipY parameters to ig.Image.draw()
 			ig.Image.inject({
-				draw: function( targetX, targetY, sourceX, sourceY, width, height, flipX, flipY ) {
-					if( !this.loaded ) { return; }
-					
+				draw: function(targetX, targetY, sourceX, sourceY, width, height, flipX, flipY) {
+					if (!this.loaded) {
+						return;
+					}
+
 					var scale = ig.system.scale;
 					sourceX = sourceX ? sourceX * scale : 0;
 					sourceY = sourceY ? sourceY * scale : 0;
 					width = (width ? width : this.width) * scale;
 					height = (height ? height : this.height) * scale;
-					
+
 					var scaleX = flipX ? -1 : 1;
 					var scaleY = flipY ? -1 : 1;
-					
-					if( flipX || flipY ) {
-						ig.system.context.save();
-						ig.system.context.scale( scaleX, scaleY );
-					}
-					
-					ig.system.context.drawImage( 
-						this.data, sourceX, sourceY, width, height,
-						ig.system.getDrawPos(targetX) * scaleX - (flipX ? width : 0), 
-						ig.system.getDrawPos(targetY) * scaleY - (flipY ? height : 0),
-						width, height
-					);
 
-					if( flipX || flipY ) {
+					if (flipX || flipY) {
+						ig.system.context.save();
+						ig.system.context.scale(scaleX, scaleY);
+					}
+
+					ig.system.context.drawImage(
+					this.data, sourceX, sourceY, width, height, ig.system.getDrawPos(targetX) * scaleX - (flipX ? width : 0), ig.system.getDrawPos(targetY) * scaleY - (flipY ? height : 0), width, height);
+
+					if (flipX || flipY) {
 						ig.system.context.restore();
 					}
-					
+
 					ig.Image.drawCount++;
 				},
 			});
 		},
 
 		draw: function() {
-			
+
 			// Check that we have entity to follow.
 			if (this.follow) {
 
@@ -79,7 +77,7 @@ ig.module('game.entities.non-weltmeister.reflection')
 				this.currentAnim.flip.y = true;
 
 			}
-			
+
 			// Remove reflection if the player does not exist.
 			else {
 
@@ -91,8 +89,8 @@ ig.module('game.entities.non-weltmeister.reflection')
 			}
 
 			// Draw entity.
-			if( this.currentAnim ) {
-				
+			if (this.currentAnim) {
+
 				var tile = this.currentAnim.tile;
 				var tileWidth = this.currentAnim.sheet.width;
 				var tileHeight = this.currentAnim.sheet.height;
@@ -104,68 +102,34 @@ ig.module('game.entities.non-weltmeister.reflection')
 				var sourceY = Math.floor((tile * tileWidth) / sheetWidth) * tileHeight;
 				var flipX = this.currentAnim.flip.x;
 				var flipY = this.currentAnim.flip.y;
-				
-				switch(this.distortionFrame) {
-					
-					// Draw normal.
-					case 0:
-						this.currentAnim.draw(
-							targetX,
-							targetY
-						);
-						break;
 
-					// Shift right-half to the right.
-					case 1:
-						this.currentAnim.sheet.image.draw(
-							(flipX ? targetX + (tileWidth / 2) : targetX),
-							targetY,
-							sourceX,
-							sourceY,
-							(tileWidth / 2) + 1,
-							tileHeight,
-							flipX,
-							flipY
-						);
+				switch (this.distortionFrame) {
 
-						this.currentAnim.sheet.image.draw(
-							(flipX ? targetX : targetX + (tileWidth / 2) + 1 ),
-							targetY,
-							sourceX + (tileWidth / 2),
-							sourceY,
-							tileWidth / 2,
-							tileHeight,
-							flipX,
-							flipY
-						);
-						break;
+				// Draw normal.
+				case 0:
+					this.currentAnim.draw(
+					targetX, targetY);
+					break;
 
-					// Shift left-half to the right.
-					case 2:
-						this.currentAnim.sheet.image.draw(
-							(flipX ? targetX + (tileWidth / 2) : targetX + 1),
-							targetY,
-							sourceX,
-							sourceY,
-							tileWidth / 2,
-							tileHeight,
-							flipX,
-							flipY
-						);
+				// Shift right-half to the right.
+				case 1:
+					this.currentAnim.sheet.image.draw(
+					(flipX ? targetX + (tileWidth / 2) : targetX), targetY, sourceX, sourceY, (tileWidth / 2) + 1, tileHeight, flipX, flipY);
 
-						this.currentAnim.sheet.image.draw(
-							(flipX ? targetX : targetX + (tileWidth / 2) ),
-							targetY,
-							sourceX + (tileWidth / 2),
-							sourceY,
-							tileWidth / 2,
-							tileHeight,
-							flipX,
-							flipY
-						);
-						break;						
+					this.currentAnim.sheet.image.draw(
+					(flipX ? targetX : targetX + (tileWidth / 2) + 1), targetY, sourceX + (tileWidth / 2), sourceY, tileWidth / 2, tileHeight, flipX, flipY);
+					break;
+
+				// Shift left-half to the right.
+				case 2:
+					this.currentAnim.sheet.image.draw(
+					(flipX ? targetX + (tileWidth / 2) : targetX + 1), targetY, sourceX, sourceY, tileWidth / 2, tileHeight, flipX, flipY);
+
+					this.currentAnim.sheet.image.draw(
+					(flipX ? targetX : targetX + (tileWidth / 2)), targetY, sourceX + (tileWidth / 2), sourceY, tileWidth / 2, tileHeight, flipX, flipY);
+					break;
 				}
-				
+
 			}
 
 			// Restore vertical flip.
@@ -173,9 +137,9 @@ ig.module('game.entities.non-weltmeister.reflection')
 		},
 
 		update: function() {
-			
+
 			// Kill this entity if player no longer exists.
-			if(this.follow._killed) this.kill();
+			if (this.follow._killed) this.kill();
 
 			// Update distortion effect.
 			//this.distortionFrame = Math.floor(this.distortionTimer.delta()) %3;
