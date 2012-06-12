fd365c993b2f5f86e3729c7af4eea464d11cbc3e
Jonathan Commins <joncom@gmail.com>
2012-06-11 22:35:45 -0700

    Modified get-collision-types.php to include reflection tiles in its output to special-tiles.js and to use their MD5'd hashes.

--- mapper/get-collision-types.php
+++ mapper/get-collision-types.php
@@ -15,7 +15,7 @@ require('inc.functions.php');
      *
      */
     
-    $interestedIn = array('grass', 'water', 'footprints');
+    $interestedIn = array('grass', 'water', 'footprints', 'reflection');
 
     // we'll need master tilesheet info to translate hashes into tile positions
     $masterTilesheetByHash = getTilesheetHashTable($globalMasterTilesheetJSON);
@@ -47,6 +47,10 @@ require('inc.functions.php');
                 $tilesByCollisionType[ $currentCollisionName ] = array();
             }
             
+            // Special case for reflection tiles, because their original tiles are on
+            // a layer other than the primary map layer.
+            if($currentCollisionName=='reflection') $tileHash = md5($tileHash);
+
             $tilePosInTilesheet = $masterTilesheetByHash[$tileHash] + $globalWMTileOffset; 
             array_push( $tilesByCollisionType[ $currentCollisionName ], $tilePosInTilesheet );
         }
