357938bf88898b28cfa0741c929630115f99079a
Jonathan Commins <joncom@gmail.com>
2012-06-13 14:54:49 -0700

    Started basic raindrop entity.

---
+++ client/lib/game/entities/non-weltmeister/raindrop.js
@@ -0,0 +1,42 @@
+ig.module('game.entities.non-weltmeister.raindrop')
+
+.requires('impact.entity')
+
+.defines(function() {
+
+	EntityRaindrop = ig.Entity.extend({
+
+		size: {
+			x: 16,
+			y: 16
+		},
+
+		// Load image resource.
+		animSheet: new ig.AnimationSheet('media/rs.raindrop.png', 16, 32),
+
+		init: function(x, y, settings) {
+			this.parent(x, y, settings);
+
+			this.vel.x = 
+		},
+
+		.handleMovementTrace( res ) {
+
+			// Do not collide with collisionMap.
+			return;
+		},
+
+		update: function() {
+
+			// Update animations.
+			if (this.currentAnim != null) this.currentAnim.update();
+
+			// Check if animation has finished.
+			if (this.currentAnim.loopCount >= 1) {
+				// Free up resources.
+				this.kill();
+			}
+		}
+
+	});
+});
