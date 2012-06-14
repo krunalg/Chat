c7cbb4819151af99f62a636fea1158611162235c
Jonathan Commins <joncom@gmail.com>
2012-06-13 20:51:10 -0700

    Fixed rain not starting completely above the screen.

--- client/lib/game/entities/non-weltmeister/weather-controller.js
+++ client/lib/game/entities/non-weltmeister/weather-controller.js
@@ -15,13 +15,11 @@ ig.module('game.entities.non-weltmeister.weather-controller')
 
 		update: function() {
 
-			// Random x.
+			// Random distance ledge screen edge.
 			var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width + ((1 / 3) * ig.system.height) - 30) ) + 30;
-			//x = Math.floor(x/32) * 32;
 
 			// Random y.
-			var y = ig.game.screen.y - Math.floor(Math.random() * 32) + 16;
-			//y = Math.floor(y/32) * 32;
+			var y = ig.game.screen.y - Math.floor(Math.random() * 16) - 32;
 
 			// Spawn a raindrop.
 			ig.game.spawnEntity(EntityRaindrop, x, y, {});
