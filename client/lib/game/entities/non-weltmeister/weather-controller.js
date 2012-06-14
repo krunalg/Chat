bb5eed2b61fdd33b87f2c87c6a7e49701ca72435
Jonathan Commins <joncom@gmail.com>
2012-06-14 15:39:20 -0700

    Created generatePos() function to eliminate duplicate code in update() function.

--- client/lib/game/entities/non-weltmeister/weather-controller.js
+++ client/lib/game/entities/non-weltmeister/weather-controller.js
@@ -32,20 +32,51 @@ ig.module('game.entities.non-weltmeister.weather-controller')
 			this.timer = new ig.Timer();
 
 			// Start sandstorm.
-			if (this.weather=='sandstorm') {
+			if (this.weather == 'sandstorm') {
 
 				// Start sand-screen.
 				this.sandscreen = ig.game.spawnEntity(EntitySandScreen, 0, 0, {});
 			}
 		},
 
+		generatePos: function() {
+
+			switch (this.weather) {
+
+			case 'rain':
+
+				// Random distance from left of screen.
+				var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width + ((1 / 3) * ig.system.height) - 30)) + 30;
+
+				// Random distance above top of screen.
+				var y = ig.game.screen.y - Math.floor(Math.random() * 16) - 32;
+
+				break;
+
+			case 'sandstorm':
+
+				// Random distance from left of screen.
+				var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width - 32));
+
+				// Start below screen.
+				var y = ig.game.screen.y + ig.system.height + radius;
+
+				break;
+			}
+
+			return {
+				x: x,
+				y: y
+			};
+		},
+
 		update: function() {
 
-			if(this.weather=='sandstorm') rate = this.sandRate;
-			else if(this.weather=='rain') rate = this.rainRate;
+			if (this.weather == 'sandstorm') rate = this.sandRate;
+			else if (this.weather == 'rain') rate = this.rainRate;
 
 			// Smoothly handle changes to rate.
-			if(this.rate!=rate) {
+			if (this.rate != rate) {
 
 				// Reset timer.
 				this.timer.set(0);
@@ -57,46 +88,25 @@ ig.module('game.entities.non-weltmeister.weather-controller')
 			// How many should be spawned this very moment?
 			var spawnCount = Math.floor(this.timer.delta() * rate);
 
-			// Rain
-			if (this.weather=='rain') {
+			// Type of entity to spawn.
+			var entityType = (this.weather == 'rain' ? EntityRaindrop : EntitySandCloud);
 
-				if( spawnCount != this.lastSpawned ) {
+			// New entities need spawning?
+			if (spawnCount != this.lastSpawned) {
 
-					// Make up for missed spawns if too many frames passed.
-					for(var i=0; i< (spawnCount - this.lastSpawned); i++) {
+				// Make up for missed spawns if too many frames passed.
+				for (var i = 0; i < (spawnCount - this.lastSpawned); i++) {
 
-						// Random distance from left of screen.
-						var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width + ((1 / 3) * ig.system.height) - 30)) + 30;
+					var position = generatePos();
 
-						// Random distance above top of screen.
-						var y = ig.game.screen.y - Math.floor(Math.random() * 16) - 32;
+					// Spawn entity.
+					ig.game.spawnEntity(entityType, position.x, position.y, {});
 
-						// Spawn a raindrop.
-						ig.game.spawnEntity(EntityRaindrop, x, y, {});
-
-					}
-
-					this.lastSpawned = spawnCount;
 				}
+
+				this.lastSpawned = spawnCount;
 			}
 
-			// Sandstorm
-			else if (this.weather=='sandstorm') {
-
-				// Random distance from left of screen.
-				var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width - 32));
-
-				// Start below screen.
-				var y = ig.game.screen.y + ig.system.height;
-
-				if( spawnCount != this.lastSpawned ) {
-
-					// Spawn a raindrop.
-					ig.game.spawnEntity(EntitySandCloud, x, y, {});
-
-					this.lastSpawned = spawnCount;
-				}
-			}
 
 			// Call parent.
 			this.parent();
