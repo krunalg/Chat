0d8c81041ce4cc46a081bd871ab446829c70d050
Jonathan Commins <joncom@gmail.com>
2012-06-14 02:14:03 -0700

    Sand-screen entity draws itselfs tiled to entire screen and repeats seemlessly.

--- client/lib/game/entities/non-weltmeister/sand-screen.js
+++ client/lib/game/entities/non-weltmeister/sand-screen.js
@@ -11,6 +11,8 @@ ig.module('game.entities.non-weltmeister.sand-screen')
 			y: 64
 		},
 
+		startPos: {x:0,y:0},
+
 		// Load image resource.
 		animSheet: new ig.AnimationSheet('media/rs.sand-screen.png', 64, 64),
 
@@ -34,11 +36,16 @@ ig.module('game.entities.non-weltmeister.sand-screen')
 
 			if( this.currentAnim ) {
 
-				for(var y=0; y<Math.ceil(ig.system.height/this.size.y); y++) {
-					for(var x=0; x<Math.ceil(ig.system.width/this.size.x); x++) {
+				this.startPos.x = Math.floor(this.pos.x % this.size.x);
+				if(this.startPos.x>0) this.startPos.x = this.startPos.x - this.size.x;
+				this.startPos.y = Math.floor(this.pos.y % this.size.y);
+				if(this.startPos.y>0) this.startPos.y = this.startPos.y - this.size.y;
+
+				for(var y=0; y<Math.ceil(ig.system.height/this.size.y)+1; y++) {
+					for(var x=0; x<Math.ceil(ig.system.width/this.size.x)+1; x++) {
 						this.currentAnim.draw(
-							this.pos.x + (x * this.size.x) - this.offset.x - ig.game._rscreen.x,
-							this.pos.y + (y * this.size.y) - this.offset.y - ig.game._rscreen.y
+							this.startPos.x + (x * this.size.x) - this.offset.x,
+							this.startPos.y + (y * this.size.y) - this.offset.y
 						);
 					}
 				}
