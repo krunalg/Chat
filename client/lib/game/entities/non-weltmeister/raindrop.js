169f1391eefe9e888b90ff7abbf53f0e1fd70151
Jonathan Commins <joncom@gmail.com>
2012-06-13 20:36:28 -0700

    Rain is randomly generated above the screen, hits the ground before leaving the screen, and auto-despawns.

--- client/lib/game/entities/non-weltmeister/raindrop.js
+++ client/lib/game/entities/non-weltmeister/raindrop.js
@@ -8,7 +8,7 @@ ig.module('game.entities.non-weltmeister.raindrop')
 
 		size: {
 			x: 16,
-			y: 16
+			y: 32
 		},
 
 		// Load image resource.
@@ -18,31 +18,53 @@ ig.module('game.entities.non-weltmeister.raindrop')
 			this.parent(x, y, settings);
 
 			// Set up animation.
-			this.addAnim('rain', 1, [0,0,0,1,2,3], true);
+			this.addAnim('fall', 1, [0], true);
+			this.addAnim('hitGround', (1 / 60), [1, 1, 1, 2, 2, 3, 3, 3], true);
 
 			// Set current animation.
-			this.currentAnim = this.anims.rain;
+			this.currentAnim = this.anims.fall;
 
 			// Set velocity.
 			this.maxVel.x = this.vel.x = -13 * 30;
 			this.maxVel.y = this.vel.y = 26 * 30;
+
+			// Moment that entity goes off-screen.
+			this.maxTimeX = ( ig.game.screen.x - this.pos.x ) / this.vel.x
+			this.maxTimeY = ( ig.system.height - this.pos.y + ig.game.screen.y - this.size.y) / this.vel.y;
+			
+			// Which happens sooner?
+			this.maxTime = (this.maxTimeX > this.maxTimeY ? this.maxTimeY : this.maxTimeX);
+
+			this.hitTimer = new ig.Timer();
+			this.hitTimer.set( Math.random() * this.maxTime );
+			
 		},
 
 		handleMovementTrace: function(res) {
 			// This completely ignores the trace result (res) and always
-		    // moves the entity according to its velocity
-		    this.pos.x += this.vel.x * ig.system.tick;
-		    this.pos.y += this.vel.y * ig.system.tick;
+			// moves the entity according to its velocity
+			this.pos.x += this.vel.x * ig.system.tick;
+			this.pos.y += this.vel.y * ig.system.tick;
 		},
 
 		update: function() {
 
-			// Check if animation has finished.
-			if (this.currentAnim.loopCount >= 1) {
-				// Free up resources.
-				this.kill();
-			}
+			if( this.hitTimer.delta() >= 0 && this.currentAnim == this.anims.fall)
+			{
+				this.vel.x = this.vel.y = 0;
+				this.currentAnim = this.anims.hitGround;
+				this.currentAnim.rewind();
+			} 
 
+			else if ( this.currentAnim == this.anims.hitGround )
+			{
+				// Check if animation has finished.
+				if (this.currentAnim.loopCount >= 1) {
+					// Free up resources.
+					this.kill();
+				}
+			}
+				
 			// Call parent.
 			this.parent();
 		}
--- client/lib/game/entities/non-weltmeister/weather-controller.js
+++ client/lib/game/entities/non-weltmeister/weather-controller.js
@@ -6,14 +6,25 @@ ig.module('game.entities.non-weltmeister.weather-controller')
 
 	EntityWeatherController = ig.Entity.extend({
 
+		// Entities per second.
+		spawnRate: 5,
+
 		init: function(x, y, settings) {
 			this.parent(x, y, settings);
 		},
 
 		update: function() {
 
+			// Random x.
+			var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width + ((1 / 3) * ig.system.height) - 30) ) + 30;
+			//x = Math.floor(x/32) * 32;
+
+			// Random y.
+			var y = ig.game.screen.y - Math.floor(Math.random() * 32) + 16;
+			//y = Math.floor(y/32) * 32;
+
 			// Spawn a raindrop.
-			ig.game.spawnEntity(EntityRaindrop, this.pos.x, this.pos.y, {});
+			ig.game.spawnEntity(EntityRaindrop, x, y, {});
 
 			// Call parent.
 			this.parent();
