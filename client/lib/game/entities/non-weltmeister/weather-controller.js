cee0b9f7cd7e69a006c153bce417454efa4e5c2b
Jonathan Commins <joncom@gmail.com>
2012-06-14 13:44:17 -0700

    WeatherController entity handles two weather types: 'rain' and 'sandstorm'.

--- client/lib/game/entities/non-weltmeister/weather-controller.js
+++ client/lib/game/entities/non-weltmeister/weather-controller.js
@@ -9,24 +9,59 @@ ig.module('game.entities.non-weltmeister.weather-controller')
 		// Max rain entities on screen at once.
 		maxRain: 16,
 
+		// Max sand-cloud entities on screen at once.
+		maxSand: 8,
+
+		// Type of weather effect.
+		weather: null,
+
 		init: function(x, y, settings) {
 			this.parent(x, y, settings);
+
+			// Start sandstorm.
+			if (this.weather=='sandstorm') {
+
+				// Start sand-screen.
+				this.sandscreen = ig.game.spawnEntity(EntitySandScreen, 0, 0, {});
+			}
 		},
 
 		update: function() {
 
-			// Random distance from left of screen.
-			var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width + ((1 / 3) * ig.system.height) - 30)) + 30;
+			// Rain
+			if (this.weather=='rain') {
 
-			// Random distance above top of screen.
-			var y = ig.game.screen.y - Math.floor(Math.random() * 16) - 32;
+				// Random distance from left of screen.
+				var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width + ((1 / 3) * ig.system.height) - 30)) + 30;
 
-			// Never exceed maximum.
-			if (ig.game.getEntitiesByType(EntityRaindrop).length < this.maxRain) {
+				// Random distance above top of screen.
+				var y = ig.game.screen.y - Math.floor(Math.random() * 16) - 32;
 
-				// Spawn a raindrop.
-				ig.game.spawnEntity(EntityRaindrop, x, y, {});
+				// Never exceed maximum.
+				if (ig.game.getEntitiesByType(EntityRaindrop).length < this.maxRain) {
+
+					// Spawn a raindrop.
+					ig.game.spawnEntity(EntityRaindrop, x, y, {});
+				}
 			}
+
+			// Sandstorm
+			else if (this.weather=='sandstorm') {
+
+				// Random distance from left of screen.
+				var x = ig.game.screen.x + Math.floor(Math.random() * (ig.system.width - 32));
+
+				// Start below screen.
+				var y = ig.game.screen.y + ig.system.height;
+
+				// Never exceed maximum.
+				if (ig.game.getEntitiesByType(EntitySandCloud).length < this.maxSand) {
+
+					// Spawn a raindrop.
+					ig.game.spawnEntity(EntitySandCloud, x, y, {});
+				}
+			}
+				
 
 			// Call parent.
 			this.parent();
