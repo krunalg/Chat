442c995cdcb5657fb9ffa775429de1fe18e5cca6
Jonathan Commins <joncom@gmail.com>
2012-06-15 17:10:57 -0700

    Added second domain to connection.php

--- client/connection.php
+++ client/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-if($_SERVER["SERVER_NAME"]!="commins.ca")
+if($_SERVER["SERVER_NAME"]!="commins.ca" && $_SERVER["SERVER_NAME"]!="pixelfuse.net")
 {
   $socketHost = '127.0.0.1';
   $socketPort = 9090;
