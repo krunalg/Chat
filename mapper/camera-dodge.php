89dde2281734b29a51bad04c6811e704b98164a6
Jonathan Commins <joncom@gmail.com>
2012-06-17 14:26:11 -0700

    Created stub camera-dodge.php

---
+++ mapper/camera-dodge.php
@@ -0,0 +1,17 @@
+<?php
+
+set_time_limit(300); // 5 minutes
+ini_set('memory_limit','512M'); 
+
+
+include('inc.globals.php');
+require('inc.functions.php');
+echo '<script type="text/javascript" src="inc.functions.js" ></script>'; // used for submitting forms
+
+
+
+
+
+
+
+?>
