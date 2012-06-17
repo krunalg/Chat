8e089718b9d90f3650bbc4e9b9fdfc05eebe90da
Jonathan Commins <joncom@gmail.com>
2012-06-17 16:01:51 -0700

    Removed a great deal of duplicated code.

--- mapper/camera-dodge-test.php
+++ mapper/camera-dodge-test.php
@@ -8,57 +8,41 @@ include('inc.globals.php');
 require('inc.functions.php');
 echo '<script type="text/javascript" src="inc.functions.js" ></script>'; // used for submitting forms
 
+
+$tests = array(
+
+    0 => array(         'x'     => 128,
+                        'y'     => 256,
+                        'state' => 'left'
+    ),
+    1 => array(         'x'     => 256,
+                        'y'     => 512,
+                        'state' => 'down'
+    ),
+    2 => array(         'x'     => 512,
+                        'y'     => 1024,
+                        'state' => 'right'
+    ),
+);
+
+
+$testCount = 3;
+$testTypes = array('write', 'delete');
+
+for($t=0; $t<count($testTypes); $t++) {
+
+    for($i=0; $i<count($tests); $i++) {
+
+        echo "<a href=\"javascript:post_to_url( 'camera-dodge.php',
+            { 
+                'action': '".$testTypes[$t]."',
+                'x': " . $tests[$i]['x'] .",
+                'y': " . $tests[$i]['y'] .",
+                'state': '" . $tests[$i]['state'] ."'
+            } );\" >Try writing $i</a><br>";        
+    }
+
+    echo "<br>";
+}   
+
 ?>
-
-
-<a href="javascript:post_to_url( 'camera-dodge.php',
-        { 
-            'action': 'write',
-            'x': 128,
-            'y': 1024,
-            'state': 'left'
-        } );" >Try writing 1.</a><br>
-
-<a href="javascript:post_to_url( 'camera-dodge.php',
-        { 
-            'action': 'write',
-            'x': 256,
-            'y': 1024,
-            'state': 'right'
-        } );" >Try writing 2.</a><br>
-
-<a href="javascript:post_to_url( 'camera-dodge.php',
-        { 
-            'action': 'write',
-            'x': 512,
-            'y': 1024,
-            'state': 'down'
-        } );" >Try writing 3.</a><br>
-
-<br>
-
-
-
-
-
-
-<a href="javascript:post_to_url( 'camera-dodge.php',
-        { 
-            'action': 'delete',
-            'x': 256,
-            'y': 1024
-        } );" >Try deleting 1.</a><br>
-
-<a href="javascript:post_to_url( 'camera-dodge.php',
-        { 
-            'action': 'delete',
-            'x': 256,
-            'y': 512
-        } );" >Try deleting 2.</a><br>
-
-<a href="javascript:post_to_url( 'camera-dodge.php',
-        { 
-            'action': 'delete',
-            'x': 128,
-            'y': 1024
-        } );" >Try deleting 3.</a><br>
