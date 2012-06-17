4988745a8037f210d5558ad0452fdcf50f1bd758
Jonathan Commins <joncom@gmail.com>
2012-06-16 21:36:50 -0700

    Performed jsFormat on camera-dodge; Added comment.

--- client/lib/game/entities/non-weltmeister/camera-dodge.js
+++ client/lib/game/entities/non-weltmeister/camera-dodge.js
@@ -7,14 +7,14 @@ ig.module('game.entities.non-weltmeister.camera-dodge')
 	EntityCameraDodge = ig.Entity.extend({
 
 		lineWidth: 4,
-		
+
 		lineColor: '#f00',
 
 		// Currently selected state.
 		limit: undefined,
 
 		// Possible camera restrictions.
-		states: ['left', 'up', 'right', 'down' ],
+		states: ['left', 'up', 'right', 'down'],
 
 		init: function(x, y, settings) {
 			this.parent(x, y, settings);
@@ -36,7 +36,7 @@ ig.module('game.entities.non-weltmeister.camera-dodge')
 			var index = this.states.indexOf(this.limit);
 
 			// Select next limit.
-			this.limit = ( index == this.states.length-1 ? this.states[0] : this.states[index + 1] );
+			this.limit = (index == this.states.length - 1 ? this.states[0] : this.states[index + 1]);
 		},
 
 		update: function() {
@@ -46,54 +46,51 @@ ig.module('game.entities.non-weltmeister.camera-dodge')
 
 		draw: function() {
 
-			switch(this.limit) {
+			switch (this.limit) {
 
-				case 'left':
+			case 'left':
 
-					var startX = this.pos.x;
-					var startY = this.pos.y;
-					var endX = startX;
-					var endY = startY + this.size.y;
-					break;
+				var startX = this.pos.x;
+				var startY = this.pos.y;
+				var endX = startX;
+				var endY = startY + this.size.y;
+				break;
 
-				case 'right':
+			case 'right':
 
-					var startX = this.pos.x + this.size.x;
-					var startY = this.pos.y;
-					var endX = startX;
-					var endY = startY + this.size.y;
-					break;
+				var startX = this.pos.x + this.size.x;
+				var startY = this.pos.y;
+				var endX = startX;
+				var endY = startY + this.size.y;
+				break;
 
-				case 'up':
+			case 'up':
 
-					var startX = this.pos.x;
-					var startY = this.pos.y;
-					var endX = startX + this.size.x;
-					var endY = startY;
-					break;
+				var startX = this.pos.x;
+				var startY = this.pos.y;
+				var endX = startX + this.size.x;
+				var endY = startY;
+				break;
 
-				case 'down':
+			case 'down':
 
-					var startX = this.pos.x;
-					var startY = this.pos.y + this.size.y;
-					var endX = startX + this.size.x;
-					var endY = startY;
-					break;
+				var startX = this.pos.x;
+				var startY = this.pos.y + this.size.y;
+				var endX = startX + this.size.x;
+				var endY = startY;
+				break;
 			}
 
+			// Set line characteristics.
 			ig.system.context.strokeStyle = this.lineColor;
 			ig.system.context.lineWidth = this.lineWidth;
 
 			// Begin drawing line along limit edge.
 			ig.system.context.beginPath();
-			ig.system.context.moveTo( 
-				ig.system.getDrawPos(startX - ig.game.screen.x),
-				ig.system.getDrawPos(startY - ig.game.screen.y)
-			);
-			ig.system.context.lineTo( 
-				ig.system.getDrawPos(endX - ig.game.screen.x),
-				ig.system.getDrawPos(endY - ig.game.screen.y)
-			);
+			ig.system.context.moveTo(
+			ig.system.getDrawPos(startX - ig.game.screen.x), ig.system.getDrawPos(startY - ig.game.screen.y));
+			ig.system.context.lineTo(
+			ig.system.getDrawPos(endX - ig.game.screen.x), ig.system.getDrawPos(endY - ig.game.screen.y));
 			ig.system.context.stroke();
 			ig.system.context.closePath();
 		}
