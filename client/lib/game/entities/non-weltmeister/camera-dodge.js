c6d61d37f2e39337893e3524c714a645c2892129
Jonathan Commins <joncom@gmail.com>
2012-06-16 21:15:01 -0700

    Added draw logic for camera-dodge.

--- client/lib/game/entities/non-weltmeister/camera-dodge.js
+++ client/lib/game/entities/non-weltmeister/camera-dodge.js
@@ -46,6 +46,55 @@ ig.module('game.entities.non-weltmeister.camera-dodge')
 
 		draw: function() {
 
+			switch(this.limit) {
+
+				case 'left':
+
+					var startX = this.pos.x;
+					var startY = this.pos.y;
+					var endX = startX;
+					var endY = startY + this.size.y;
+					break;
+
+				case 'right':
+
+					var startX = this.pos.x + this.size.x;
+					var startY = this.pos.y;
+					var endX = startX;
+					var endY = startY + this.size.y;
+					break;
+
+				case 'up':
+
+					var startX = this.pos.x;
+					var startY = this.pos.y;
+					var endX = startX + this.size.x;
+					var endY = startY;
+					break;
+
+				case 'down':
+
+					var startX = this.pos.x;
+					var startY = this.pos.y + this.size.y;
+					var endX = startX + this.size.x;
+					var endY = startY;
+					break;
+			}
+
+			ig.system.context.strokeStyle = this.lineColor;
+			ig.system.context.lineWidth = this.lineWidth;
+
+			ig.system.context.beginPath();
+			ig.system.context.moveTo( 
+				ig.system.getDrawPos(startX - ig.game.screen.x),
+				ig.system.getDrawPos(startY - ig.game.screen.y)
+			);
+			ig.system.context.lineTo( 
+				ig.system.getDrawPos(endX - ig.game.screen.x),
+				ig.system.getDrawPos(endY - ig.game.screen.y)
+			);
+			ig.system.context.stroke();
+			ig.system.context.closePath();
 		}
 
 
