17570a3b4e8e00776bd37d06a30e80addbb982e1
Jonathan Commins <joncom@gmail.com>
2012-06-17 13:43:11 -0700

    Camera-dodge entities use index to know what state is current; 'limit' property is now an object with x and y calculated internally instead of externally.

--- client/lib/game/entities/non-weltmeister/camera-dodge.js
+++ client/lib/game/entities/non-weltmeister/camera-dodge.js
@@ -11,10 +11,16 @@ ig.module('game.entities.non-weltmeister.camera-dodge')
 		lineColor: '#f00',
 
 		// Currently selected state.
-		limit: undefined,
+		limit: {
+			x: undefined,
+			y: undefined
+		},
 
 		// Possible camera restrictions.
 		states: ['left', 'up', 'right', 'down'],
+
+		// Index of current state.
+		index: 0,
 
 		init: function(x, y, settings) {
 			this.parent(x, y, settings);
@@ -25,76 +31,100 @@ ig.module('game.entities.non-weltmeister.camera-dodge')
 			// Set cursor size.
 			this.size.x = this.size.y = this.tilesize;
 
-			// Set current limit.
-			this.limit = this.states[0];
+			// Set first limit.
+			this.next();
 		},
 
 		// Change to next possible state.
 		next: function() {
 
-			// Index of current limit.
-			var index = this.states.indexOf(this.limit);
+			// Select next state.
+			this['index'] = ((this['index'] == this.states.length - 1) ? 0 : this['index'] + 1;
 
-			// Select next limit.
-			this.limit = (index == this.states.length - 1 ? this.states[0] : this.states[index + 1]);
-		},
-
-		update: function() {
-
-			this.parent();
-		},
-
-		draw: function() {
-
-			switch (this.limit) {
-
-			case 'left':
-
-				var startX = this.pos.x;
-				var startY = this.pos.y;
-				var endX = startX;
-				var endY = startY + this.size.y;
-				break;
-
-			case 'right':
-
-				var startX = this.pos.x + this.size.x;
-				var startY = this.pos.y;
-				var endX = startX;
-				var endY = startY + this.size.y;
-				break;
+			switch (this.states[this['index']]) {
 
 			case 'up':
 
-				var startX = this.pos.x;
-				var startY = this.pos.y;
-				var endX = startX + this.size.x;
-				var endY = startY;
+				this.limit.y = this.pos.y;
+				this.limit.x = undefined;
 				break;
 
 			case 'down':
 
-				var startX = this.pos.x;
-				var startY = this.pos.y + this.size.y;
-				var endX = startX + this.size.x;
-				var endY = startY;
+				this.limit.y = this.pos.y + this.size.y;
+				this.limit.x = undefined;
+				break;
+
+			case 'left':
+
+				this.limit.x = this.pos.x;
+				this.limit.y = undefined;
+				break;
+
+			case 'right':
+
+				this.limit.x = this.pos.x + this.size.x;
+				this.limit.y = undefined;
 				break;
 			}
+			},
 
-			// Set line characteristics.
-			ig.system.context.strokeStyle = this.lineColor;
-			ig.system.context.lineWidth = this.lineWidth;
+			update: function() {
 
-			// Begin drawing line along limit edge.
-			ig.system.context.beginPath();
-			ig.system.context.moveTo(
-			ig.system.getDrawPos(startX - ig.game.screen.x), ig.system.getDrawPos(startY - ig.game.screen.y));
-			ig.system.context.lineTo(
-			ig.system.getDrawPos(endX - ig.game.screen.x), ig.system.getDrawPos(endY - ig.game.screen.y));
-			ig.system.context.stroke();
-			ig.system.context.closePath();
-		}
+				this.parent();
+			},
+
+			draw: function() {
+
+				switch (this.states[this['limit']]) {
+
+				case 'left':
+
+					var startX = this.pos.x;
+					var startY = this.pos.y;
+					var endX = startX;
+					var endY = startY + this.size.y;
+					break;
+
+				case 'right':
+
+					var startX = this.pos.x + this.size.x;
+					var startY = this.pos.y;
+					var endX = startX;
+					var endY = startY + this.size.y;
+					break;
+
+				case 'up':
+
+					var startX = this.pos.x;
+					var startY = this.pos.y;
+					var endX = startX + this.size.x;
+					var endY = startY;
+					break;
+
+				case 'down':
+
+					var startX = this.pos.x;
+					var startY = this.pos.y + this.size.y;
+					var endX = startX + this.size.x;
+					var endY = startY;
+					break;
+				}
+
+				// Set line characteristics.
+				ig.system.context.strokeStyle = this.lineColor;
+				ig.system.context.lineWidth = this.lineWidth;
+
+				// Begin drawing line along limit edge.
+				ig.system.context.beginPath();
+				ig.system.context.moveTo(
+				ig.system.getDrawPos(startX - ig.game.screen.x), ig.system.getDrawPos(startY - ig.game.screen.y));
+				ig.system.context.lineTo(
+				ig.system.getDrawPos(endX - ig.game.screen.x), ig.system.getDrawPos(endY - ig.game.screen.y));
+				ig.system.context.stroke();
+				ig.system.context.closePath();
+			}
 
 
+		});
 	});
-});
