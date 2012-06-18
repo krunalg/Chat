763a1a5652d90a7cf0532e896035e0e07058b1a6
Jonathan Commins <joncom@gmail.com>
2012-06-17 20:42:08 -0700

    Splash entity logic in place.

--- client/lib/game/entities/non-weltmeister/splash.js
+++ client/lib/game/entities/non-weltmeister/splash.js
@@ -1,3 +1,5 @@
+/* Do the splash and jump entities share enough in common to share a single parent entity? */
+
 ig.module('game.entities.non-weltmeister.splash')
 
 .requires('impact.entity')
@@ -11,15 +13,32 @@ ig.module('game.entities.non-weltmeister.splash')
 			y: 16
 		},
 
-		// Reference of entity of which to follow.
-		follow: null,
-
-		// Initialize
-		init: function(x, y, settings) {
-			this.parent(x, y, settings);
+		offset: {
+			x: 0,
+			y: -8
 		},
 
-		update: function() {
+		// Used for fetching position.
+		player: null,
+
+		// Load image resource.
+		animSheet: new ig.AnimationSheet('media/rs.splash.png', 16, 8),
+
+		init: function(x, y, settings) {
+			this.parent(x, y, settings);
+
+			// Define animation sequence.
+			this.addAnim('spritz', (2 / 60), [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1]);
+
+			// Set current animation.
+			this.currentAnim = this.anims.spritz;
+		},
+
+		draw: function() {
+
+			// Follow the player.
+			this.pos = this.player.pos;
+
 			this.parent();
 		}
 
