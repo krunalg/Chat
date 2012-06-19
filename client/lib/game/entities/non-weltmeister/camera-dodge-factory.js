54e06d95b56b7e29910b70db2d13a53acb5db657
Jonathan Commins <joncom@gmail.com>
2012-06-18 22:15:18 -0700

    Camera-dodge-factory spawns all Ajax-retrieved camera-dodges on init().

--- client/lib/game/entities/non-weltmeister/camera-dodge-factory.js
+++ client/lib/game/entities/non-weltmeister/camera-dodge-factory.js
@@ -9,6 +9,12 @@ ig.module('game.entities.non-weltmeister.camera-dodge-factory')
 		// Where to read, write, and delete camera dodge info via AJAX.
 		ajaxURL: 'http://127.0.0.1/pokemon-chat/mapper/camera-dodge.php',
 
+		// 2D array populated via Ajax.
+		buildMe: undefined,
+
+		// Used for one time building entities.
+		built: false,
+
 		init: function(x, y, settings) {
 			this.parent(x, y, settings);
 
@@ -20,18 +26,34 @@ ig.module('game.entities.non-weltmeister.camera-dodge-factory')
 			  	dataType: "json"
 			});
 
-			request.done(function(msg) {
-			  	console.log('Ajax success: ' + msg);
+			request.done(function(json) {
+			  	ig.game.cdFactory.buildMe = json;
+			  	console.log('Ajax successful: camera-dodge-factory init()');
 			});
 
 			request.fail(function(jqXHR, textStatus) {
-			  	console.log('Ajax failed:' + textStatus);
+				console.log('Ajax failed: camera-dodge-factory init() ' + textStatus);
+				this.kill();
 			});
 		},
 
 		update: function() {
 
 			this.parent();
+
+			if(!this.built && typeof this.buildMe != 'undefined') {
+				
+				for(var x in this.buildMe) {
+				
+					for(var y in this.buildMe[x]) {
+
+						var cameraDodge = ig.game.spawnEntity(EntityCameraDodge, x, y);
+						cameraDodge['index'] = cameraDodge.states.indexOf(this.buildMe[x][y]);
+					}
+				}
+
+				this.built = true;
+			}
 		}
 
 
