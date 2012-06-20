aa58a93ced139c71d5c4b078241e4ee1517fd39f
Jonathan Commins <joncom@gmail.com>
2012-06-19 22:16:09 -0700

    Re-organized for readibility.

--- mapper/merge-map-tiles.php
+++ mapper/merge-map-tiles.php
@@ -52,113 +52,111 @@ else if( (isset($_POST['build']) && $_POST['build']=='all') || $automate )
     $tiles = scanFileNameRecursivly($globalTileDumpDir);
     
     // only perform a merge if there is more than one file
-    if(count($tiles>1))
-    {
-        // build array of important collision types
-        // for special cases such as tiles which need to be above player
-        $collisionIndex = 0;
-        $indexOfCollision = array(); // holds special cases
-        foreach($globalCollisions as $index => $collision)
-        {
-            $indexOfCollision[$index] = $collisionIndex;
-            $collisionIndex++;
-        }
-        
-        // we need to open the tile states file and find out which
-        // tiles have been marked as special
-        $savedCollisions = getCollisionsFromFile($globalCollisionsFile);
-        $savedCollisions = buildHashIndexCollisions($savedCollisions);
-        foreach($savedCollisions as $hash => $collision)
-        {
-            // Identify special-case tiles.
-            if($collision==$indexOfCollision['above'] || $collision==$indexOfCollision['reflection'])
-            {
-                // Set folder where to rip tiles to.
-                if($collision==$indexOfCollision['above']) $directory = $globalAboveDumpDir;
-                else if($collision==$indexOfCollision['reflection']) $directory = $globalBelowDumpDir;
+    if(count($tiles<=1)) die("Not enough tiles to perform a merge.");
 
-                // remember that we md5 the name of the special tile because
-                // it has to be a different name than the original tile,
-                // and still be easily findable.
-                $collisionTilePath = $directory .
-                    DIRECTORY_SEPARATOR . md5($hash) . '.png';
-                array_push($tiles, $collisionTilePath);
-            }
-        }
-        
-        // calculate master tilesheet dimensions
-        $tilesheetWidthInTiles = $globalMasterTilesheetWidth/$globalTilesize;
-        $tilesheetHeightInTiles = ceil(count($tiles)/$tilesheetWidthInTiles);
-            //echo "w: $tilesheetWidthInTiles ... h: $tilesheetHeightInTiles ";
-        
-        // create new image
-        $newimg = // create empty tilesheet image
-            imagecreatetruecolor(
-                $tilesheetWidthInTiles * $globalTilesize,
-                $tilesheetHeightInTiles * $globalTilesize );
-        
-        // we want to save alpha channel
-        imagealphablending($newimg, false);
-        imagesavealpha($newimg, true);
-        
-        // adding tiles to master tilesheet
-        $nextTile = 0; // progresses through tiles array index
-        for($y=0; $y<$tilesheetHeightInTiles; $y++)
-        {
-            for($x=0; $x<$tilesheetWidthInTiles; $x++)
-            {
-                $tile = LoadPNG($tiles[$nextTile]);
-                
-                // regardless of tile's real size (which should always match
-                // the global tilesize) we will use global tilesize
-                
-                // attempt to copy current tile into master tilesheet
-                if(!imagecopy( 
-                    $newimg, // destination image
-                    $tile, // source image
-                    $x*$globalTilesize, $y*$globalTilesize, // x, y destination
-                    0, 0, // x, y source
-                    $globalTilesize, // copy width
-                    $globalTilesize // copy height
-                )) die( "".$tiles[$nextTile].' <b>failed</b>. '.
-                        'Could not copy tile.'  );
-                
-                // stop copy tiles when there are none left to write
-                if($nextTile==count($tiles)-1)
-                {
-                    break;
-                }
-                $nextTile++; // otherwise next tile
-            }
-        }
-        
-        // attempt to write master tilesheet to disk
-        if(!imagepng($newimg, $globalMasterTilesheetFile))
-            die( '<b style="color:red">Failed</b> writing file: '.
-                 $globalMasterTilesheetFile );
-        else
-            echo "<b>Success</b> writing file: ".$globalMasterTilesheetFile."<br>\n";
-            
-        
-        $justHashes = array();
-        for($i=0; $i<count($tiles); $i++)
-        {
-            $splitAtSlashes = explode(DIRECTORY_SEPARATOR, $tiles[$i]);
-            $justFilename = $splitAtSlashes[ count($splitAtSlashes)-1 ];
-            $justHash = substr($justFilename, 0, count($justFilename)-5 ); // cuts off '.png'
-            array_push($justHashes, $justHash);
-        }
-        $afterJSON = json_encode($justHashes); // need to save this
-        
-        // write to file
-        if(!file_put_contents($globalMasterTilesheetJSON, $afterJSON))
-            die( '<b style="color:red">Failed</b> writing file: '.
-                 $globalMasterTilesheetJSON );
-        else
-            echo "<b>Success</b> writing file: " . $globalMasterTilesheetJSON;
-        
+    // build array of important collision types
+    // for special cases such as tiles which need to be above player
+    $collisionIndex = 0;
+    $indexOfCollision = array(); // holds special cases
+    foreach($globalCollisions as $index => $collision)
+    {
+        $indexOfCollision[$index] = $collisionIndex;
+        $collisionIndex++;
     }
-    else die("Not enough tiles to perform a merge.");
+    
+    // we need to open the tile states file and find out which
+    // tiles have been marked as special
+    $savedCollisions = getCollisionsFromFile($globalCollisionsFile);
+    $savedCollisions = buildHashIndexCollisions($savedCollisions);
+    foreach($savedCollisions as $hash => $collision)
+    {
+        // Identify special-case tiles.
+        if($collision==$indexOfCollision['above'] || $collision==$indexOfCollision['reflection'])
+        {
+            // Set folder where to rip tiles to.
+            if($collision==$indexOfCollision['above']) $directory = $globalAboveDumpDir;
+            else if($collision==$indexOfCollision['reflection']) $directory = $globalBelowDumpDir;
+
+            // remember that we md5 the name of the special tile because
+            // it has to be a different name than the original tile,
+            // and still be easily findable.
+            $collisionTilePath = $directory .
+                DIRECTORY_SEPARATOR . md5($hash) . '.png';
+            array_push($tiles, $collisionTilePath);
+        }
+    }
+    
+    // calculate master tilesheet dimensions
+    $tilesheetWidthInTiles = $globalMasterTilesheetWidth/$globalTilesize;
+    $tilesheetHeightInTiles = ceil(count($tiles)/$tilesheetWidthInTiles);
+        //echo "w: $tilesheetWidthInTiles ... h: $tilesheetHeightInTiles ";
+    
+    // create new image
+    $newimg = // create empty tilesheet image
+        imagecreatetruecolor(
+            $tilesheetWidthInTiles * $globalTilesize,
+            $tilesheetHeightInTiles * $globalTilesize );
+    
+    // we want to save alpha channel
+    imagealphablending($newimg, false);
+    imagesavealpha($newimg, true);
+    
+    // adding tiles to master tilesheet
+    $nextTile = 0; // progresses through tiles array index
+    for($y=0; $y<$tilesheetHeightInTiles; $y++)
+    {
+        for($x=0; $x<$tilesheetWidthInTiles; $x++)
+        {
+            $tile = LoadPNG($tiles[$nextTile]);
+            
+            // regardless of tile's real size (which should always match
+            // the global tilesize) we will use global tilesize
+            
+            // attempt to copy current tile into master tilesheet
+            if(!imagecopy( 
+                $newimg, // destination image
+                $tile, // source image
+                $x*$globalTilesize, $y*$globalTilesize, // x, y destination
+                0, 0, // x, y source
+                $globalTilesize, // copy width
+                $globalTilesize // copy height
+            )) die( "".$tiles[$nextTile].' <b>failed</b>. '.
+                    'Could not copy tile.'  );
+            
+            // stop copy tiles when there are none left to write
+            if($nextTile==count($tiles)-1)
+            {
+                break;
+            }
+            $nextTile++; // otherwise next tile
+        }
+    }
+    
+    // attempt to write master tilesheet to disk
+    if(!imagepng($newimg, $globalMasterTilesheetFile))
+        die( '<b style="color:red">Failed</b> writing file: '.
+             $globalMasterTilesheetFile );
+    else
+        echo "<b>Success</b> writing file: ".$globalMasterTilesheetFile."<br>\n";
+        
+    
+    $justHashes = array();
+    for($i=0; $i<count($tiles); $i++)
+    {
+        $splitAtSlashes = explode(DIRECTORY_SEPARATOR, $tiles[$i]);
+        $justFilename = $splitAtSlashes[ count($splitAtSlashes)-1 ];
+        $justHash = substr($justFilename, 0, count($justFilename)-5 ); // cuts off '.png'
+        array_push($justHashes, $justHash);
+    }
+    $afterJSON = json_encode($justHashes); // need to save this
+    
+    // write to file
+    if(!file_put_contents($globalMasterTilesheetJSON, $afterJSON))
+        die( '<b style="color:red">Failed</b> writing file: '.
+             $globalMasterTilesheetJSON );
+    else
+        echo "<b>Success</b> writing file: " . $globalMasterTilesheetJSON;
+    
 }
 
 ?>
