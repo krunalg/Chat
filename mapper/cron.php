350ee3074186a695d85a9255926e38d9fad056ad
Jonathan Commins <joncom@gmail.com>
2012-06-19 19:50:15 -0700

    Report time-elapsed.

--- mapper/cron.php
+++ mapper/cron.php
@@ -13,14 +13,31 @@ function rrmdir($dir) {
     rmdir($dir);
 }
 
+# used to calculate time elapsed
+function getTime() { 
+	$a = explode (' ',microtime()); 
+	return(double) $a[0] + $a[1]; 
+} 
+
+
+
+
+
 $automate = true;
 
 // Empty the build directory.
 if(is_dir($buildDir)) rrmdir($buildDir);
 mkdir($buildDir);
 
+$Start = getTime();
 require('dump-map-tiles.php');
+$End = getTime(); 
+echo "<br><br>Time taken = ".number_format(($End - $Start),2)." secs";
+
+$Start = getTime();
 require('merge-maps.php');
+$End = getTime(); 
+echo "<br><br>Time taken = ".number_format(($End - $Start),2)." secs";
 
 
 ?>
