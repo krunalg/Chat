bb93e92f98cac2e4b278bfb60455b50b83524f19
Jonathan Commins <joncom@gmail.com>
2012-06-23 01:44:01 -0700

    PHP script nessesary for writing.

--- client/lib/game/entities/non-weltmeister/camera-dodge-factory.js
+++ client/lib/game/entities/non-weltmeister/camera-dodge-factory.js
@@ -7,7 +7,7 @@ ig.module('game.entities.non-weltmeister.camera-dodge-factory')
 	EntityCameraDodgeFactory = ig.Entity.extend({
 
 		// Where to read, write, and delete camera dodge info via AJAX.
-		ajaxURL: 'lib/game/saved-camera-dodges.js',
+		ajaxURL: 'http://127.0.0.1/pokemon-chat/mapper/camera-dodge.php',
 
 		// 2D array populated via Ajax.
 		buildMe: undefined,
