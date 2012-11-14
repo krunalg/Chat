f3360edc772e33672b3dd6141e404bfd2209fcab
Jonathan Commins <joncom@gmail.com>
2012-11-14 00:51:57 -0800

    Set class C IP explicitly to allow for local network testing.

--- client/connection.php
+++ client/connection.php
@@ -1,14 +1,17 @@
 <?php
 
 
-if($_SERVER["SERVER_NAME"]!="commins.ca" && $_SERVER["SERVER_NAME"]!="pixelfuse.net")
+if( $_SERVER["SERVER_NAME"]!="commins.ca"
+	&& $_SERVER["SERVER_NAME"]!="pixelfuse.net"
+	&& $_SERVER["SERVER_NAME"]!="joncom.no-ip.org"
+	)
 {
-  $socketHost = '127.0.0.1';
+  $socketHost = '192.168.1.65';
   $socketPort = 9090;
 }
 else
 {
-  $socketHost = 'h.commins.ca';
+  $socketHost = 'joncom.no-ip.org';
   $socketPort = 9090;
 }
 
