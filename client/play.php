e7fd306f2d1c661a397afec75279d765b741b954
Jonathan Commins <joncom@gmail.com>
2012-12-06 21:58:54 -0800

    Renamed variables.

--- client/play.php
+++ client/play.php
@@ -1,7 +1,9 @@
 <?php
 
-$socketHost = $_GET['host'];
-$socketPort = $_GET['port'];
+// Socket Server
+$host = $_GET['host'];
+$port = $_GET['port'];
+
 $user = $_GET['user'];
 
 ?>
@@ -13,7 +15,7 @@ $user = $_GET['user'];
 
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js" ></script>
 
-    <script type="text/javascript" src="http://<?php echo $socketHost . ':' . $socketPort; ?>/socket.io/socket.io.js"></script>
+    <script type="text/javascript" src="http://<?php echo $host . ':' . $port; ?>/socket.io/socket.io.js"></script>
 
     <script type="text/javascript" src="lib/impact/impact.js"></script>
 
