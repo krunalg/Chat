7368f44388ce130a4f6b45e6aeddfc5cca5177b2
Jonathan Commins <joncom@gmail.com>
2012-12-06 21:06:29 -0800

    Converted tabs to spaces; Spaced lines.

--- client/play.php
+++ client/play.php
@@ -5,23 +5,41 @@ $socketPort = $_GET['port'];
 
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-	 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js" ></script>
-	 <script type="text/javascript" src="http://<?php echo $socketHost . ':' . $socketPort; ?>/socket.io/socket.io.js"></script>
-	 <script type="text/javascript" src="lib/impact/impact.js"></script>
-	 <script type="text/javascript" src="lib/game/main.js"></script>
-	 <script type="text/javascript" src="socketClient.js"></script>
 
-	 <title>flaming-sansa</title>
-	 <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<html>
+
+<head>
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js" ></script>
+
+    <script type="text/javascript" src="http://<?php echo $socketHost . ':' . $socketPort; ?>/socket.io/socket.io.js"></script>
+
+    <script type="text/javascript" src="lib/impact/impact.js"></script>
+
+    <script type="text/javascript" src="lib/game/main.js"></script>
+
+    <script type="text/javascript" src="socketClient.js"></script>
+
+    <title>flaming-sansa</title>
+
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
 </head>
+
 <body>
-	<canvas id="canvas"></canvas>
-	<div id="chat">
-		  <div>
-			   <input type="text" id="input" autocomplete="off"></input>
-		  </div>
-	</div>
+
+    <canvas id="canvas"></canvas>
+
+    <div id="chat">
+
+        <div>
+
+            <input type="text" id="input" autocomplete="off"></input>
+
+        </div>
+
+    </div>
+
 </body>
+
 </html>
