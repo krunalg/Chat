d8fd38e5b2c8950fec4c487f2e2fab8ba988612b
Jonathan Commins <joncom@gmail.com>
2012-12-07 22:48:46 -0800

    No need to set vars.

--- api/application/models/user_model.php
+++ api/application/models/user_model.php
@@ -21,19 +21,8 @@ class User_model extends CI_Model {
 
         $query = $this->db->get_where( 'users', array('id' => $id) );
 
-        $result = $query->result();
-        $result = $result[0];
-
-        $this->id     = $id;
-        $this->user   = $result->user;
-        $this->x      = $result->x;
-        $this->y      = $result->y;
-        $this->facing = $result->facing;
-        $this->skin   = $result->skin;
-        $this->state  = $result->state;
-        $this->map    = $result->map;
-
-        return $result;
+        // Return the first in the array.
+        return array_shift( $query->result() );
 
     }
 
