d69df3faa055c680c24444f7df0b9d0a73577027
Jonathan Commins <joncom@gmail.com>
2012-12-07 22:00:12 -0800

    Started user module.

---
+++ CodeIgniter_2.1.3/application/controllers/user.php
@@ -0,0 +1,32 @@
+<?php
+class Player extends CI_Controller {
+
+	function index() {
+
+		$this->load->model('User_model');
+
+		$players = $this->User_model->read();
+
+		print_r($players);
+		/*
+		$data['todo_list'] = array('Clean House', 'Call Mom', 'Run Errands');
+
+		$data['title'] = "My Real Title";
+		$data['heading'] = "My Real Heading";
+
+		$this->load->view('blogview', $data, true);
+		*/
+
+	}
+
+	function lookup() {
+
+		$this->load->model('Player_model');
+
+		$player = $this->Player_model->read();
+
+		print_r( $player );
+
+	}
+}
+?>
---
+++ CodeIgniter_2.1.3/application/models/user_model.php
@@ -0,0 +1,44 @@
+<?php
+
+class Player_model extends CI_Model {
+
+    var $title   = '';
+    var $content = '';
+    var $date    = '';
+
+    function __construct() {
+
+        parent::__construct();
+
+    }
+
+    function read() {
+
+        $query = $this->db->get('users', 10);
+        return $query->result();
+
+    }
+
+    function insert() {
+
+        $this->title   = $_POST['title']; // please read the below note
+        $this->content = $_POST['content'];
+        $this->date    = time();
+
+        $this->db->insert('entries', $this);
+
+    }
+
+    function update() {
+
+        $this->title   = $_POST['title'];
+        $this->content = $_POST['content'];
+        $this->date    = time();
+
+        $this->db->update('entries', $this, array('id' => $_POST['id']));
+
+    }
+
+}
+
+?>
