b236adf455ffc609d9fd677dcc34cc6783df5ff4
Jonathan Commins <joncom@gmail.com>
2012-12-07 21:52:20 -0800

    Added ID to users table and made primary key.

--- database.sql
+++ database.sql
@@ -21,6 +21,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(11) NOT NULL COMMENT 'in-game name',
   `x` int(11) NOT NULL DEFAULT '0' COMMENT 'last recorded x position',
   `y` int(11) NOT NULL DEFAULT '0' COMMENT 'last recorded y position',
@@ -28,13 +29,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `skin` varchar(20) NOT NULL,
   `state` varchar(20) NOT NULL,
   `map` varchar(30) NOT NULL,
-  PRIMARY KEY (`user`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` VALUES('Jake', 416, 2960, 'up', 'boy', 'idle', 'RsWorld');
-INSERT INTO `users` VALUES('Joncom', 416, 2960, 'up', 'girl', 'idle', 'RsWorld');
-INSERT INTO `users` VALUES('kitti', 256, 224, 'right', 'boy', 'idle', 'RsBattleTower');
+INSERT INTO `users` (`user`, `x`, `y`, `facing`, `skin`, `state`, `map`) VALUES
+('Jake', 416, 2960, 'up', 'boy', 'idle', 'RsWorld'),
+('Joncom', 416, 2960, 'up', 'girl', 'idle', 'RsWorld'),
+('kitti', 256, 224, 'right', 'boy', 'idle', 'RsBattleTower');
