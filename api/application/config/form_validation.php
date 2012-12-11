7f8889c2e52060a1a644ae2deab5d9e0948dc31f
Jonathan Commins <joncom@gmail.com>
2012-12-11 00:52:46 -0800

    Defined validation rules for adding a user.

--- api/application/config/form_validation.php
+++ api/application/config/form_validation.php
@@ -76,27 +76,76 @@ $config = array(
 
     ),
 
-    'example_form' => array(
+    'add_user' => array(
 
         array(
-        'field' => 'username',
-        'label' => 'Username',
-        'rules' => 'required'
+
+            'field' => 'user',
+
+            'label' => "username",
+
+            'rules' => 'required|is_unique[users.user]|min_length[3]|max_length[12]'
+
         ),
+
         array(
-        'field' => 'password',
-        'label' => 'Password',
-        'rules' => 'required'
+
+            'field' => 'x',
+
+            'label' => 'X coordinate',
+
+            'rules' => 'integer'
+
         ),
+
         array(
-        'field' => 'passconf',
-        'label' => 'PasswordConfirmation',
-        'rules' => 'required'
+
+            'field' => 'y',
+
+            'label' => 'Y coordinate',
+
+            'rules' => 'integer'
+
         ),
+
         array(
-        'field' => 'email',
-        'label' => 'Email',
-        'rules' => 'required'
+
+            'field' => 'facing',
+
+            'label' => 'direction faced',
+
+            'rules' => 'is_direction'
+
+        ),
+
+        array(
+
+            'field' => 'skin',
+
+            'label' => 'skin',
+
+            'rules' => 'alpha'
+
+        ),
+
+        array(
+
+            'field' => 'state',
+
+            'label' => 'movement state',
+
+            'rules' => 'alpha'
+
+        ),
+
+        array(
+
+            'field' => 'map',
+
+            'label' => 'current zone',
+
+            'rules' => 'alpha'
+
         )
 
     )
