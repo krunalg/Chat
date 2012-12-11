464ffbb9a508f5aa44d22654b980778eefd8bc93
Jonathan Commins <joncom@gmail.com>
2012-12-10 22:34:35 -0800

    Added comment.

--- api/application/controllers/users.php
+++ api/application/controllers/users.php
@@ -172,6 +172,8 @@ class Users extends CI_Controller {
 
         if( $code == 201 ) {
 
+            // Special case requires URL of newly created resource.
+
             $this->output->set_header("HTTP/1.1 201 Created");
 
             $this->output->set_header("Location: $location");
