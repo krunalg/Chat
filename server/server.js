98f4862cb46a7e9229b83ee583d4f8003451549e
Jonathan Commins <joncom@gmail.com>
2012-12-11 14:21:50 -0800

    Add a new line after each message.

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ io.set('log level', 1);
 // Log message to file.
 var logToFile = function( message ) {
 
-    var line = getTime() + ' ' + message;
+    var line = getTime() + ' ' + message + "\n";
 
     //fs = require('fs');
     fs.appendFile('chat.log', line, function (err) {
