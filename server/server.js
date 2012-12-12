942afadd852151008f8c50398cd36f84e8e1831e
Jonathan Commins <joncom@gmail.com>
2012-12-12 00:37:50 -0800

    Not interested in headers.

--- server/server.js
+++ server/server.js
@@ -501,8 +501,6 @@ io.sockets.on('connection', function(socket) {
 
             console.log('STATUS: ' + res.statusCode);
 
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-
             res.setEncoding('utf8');
 
             res.on('data', function (chunk) {
