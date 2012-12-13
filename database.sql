7efa9d0e4d7eaa56b3ecd70c3eca51a28988fc26
Jonathan Commins <joncom@gmail.com>
2012-12-13 01:21:53 -0800

    Added required comma.

--- database.sql
+++ database.sql
@@ -84,6 +84,6 @@ CREATE TABLE IF NOT EXISTS `monsters` (
   `base_sp_def` int(11) NOT NULL COMMENT 'Base Special Defense',
   `base_spd` int(11) NOT NULL COMMENT 'Base Speed',
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`type_2`) REFERENCES monster_types(`id`)
+  FOREIGN KEY (`type_2`) REFERENCES monster_types(`id`),
   KEY `hoenn_id` (`hoenn_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
