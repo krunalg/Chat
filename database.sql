2a27b122091a8d2ec8e58886085baa7e4fdc53b8
Jonathan Commins <joncom@gmail.com>
2012-12-13 01:21:36 -0800

    Removed unpreferred method of having type_1 as foreign key.

--- database.sql
+++ database.sql
@@ -83,9 +83,6 @@ CREATE TABLE IF NOT EXISTS `monsters` (
   `base_sp_atk` int(11) NOT NULL COMMENT 'Base Special Attack',
   `base_sp_def` int(11) NOT NULL COMMENT 'Base Special Defense',
   `base_spd` int(11) NOT NULL COMMENT 'Base Speed',
-  CONSTRAINT test FOREIGN KEY (type_1)
-    REFERENCES monster_types(id)
-    ON DELETE CASCADE,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`type_2`) REFERENCES monster_types(`id`)
   KEY `hoenn_id` (`hoenn_id`)
