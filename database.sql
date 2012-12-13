5ca10e866305e54e4522d2adb9587c04938113c7
Jonathan Commins <joncom@gmail.com>
2012-12-13 14:09:24 -0800

    Created 'attack_categories' table.

--- database.sql
+++ database.sql
@@ -703,6 +703,25 @@ INSERT INTO `monster_exp` (`level`, `group`, `next_level`, `total`) VALUES
 (100, 'slow', 0, 1250000);
 
 --
+-- Table structure for table `attack_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `attack_categories` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `attack_categories`
+--
+
+INSERT INTO `attack_categories` (`id`, `name`) VALUES
+(1, 'physical'),
+(2, 'special'),
+(3, 'status');
+
+--
 -- Table structure for table `monsters`
 --
 
