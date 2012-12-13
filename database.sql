3efb07cd2af2cef1957c740d32aa5acc364a2c37
Jonathan Commins <joncom@gmail.com>
2012-12-13 14:13:18 -0800

    Set charset to UTF8.

--- database.sql
+++ database.sql
@@ -710,7 +710,7 @@ CREATE TABLE IF NOT EXISTS `attack_categories` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=UTF8 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `attack_categories`
