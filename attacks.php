99ac6278f5be01080d654b6dae29442df6ab174a
Jonathan Commins <joncom@gmail.com>
2012-12-14 00:15:22 -0800

    Added incomplete conversation table for attack_types.

--- attacks.php
+++ attacks.php
@@ -317,6 +317,44 @@ Status
 
 $cat_id = Array( 'Special' => 1, 'Status' => 2, 'Physical' => 3 );
 
+$type_id = Array(
+
+	'bug' => 1,
+
+	'dark' => 1,
+
+	'dragon' => 1,
+
+	'electric' => 1,
+
+	'fighting' => 1,
+
+	'fire' => 1,
+
+	'flying' => 1,
+
+	'ghost' => 1,
+
+	'grass' => 1,
+
+	'ground' => 1,
+
+	'ice' => 1,
+
+	'normal' => 1,
+
+	'poison' => 1,
+
+	'psychic' => 1,
+
+	'rock' => 1,
+
+	'steel' => 1,
+
+	'water' => 1,
+
+);
+
 // Begin extracting attacks.
 
 $attacks = explode( "\n", $attacks );
