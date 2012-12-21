b508fa24133d627f1304b4699be3bb7b29e2fa2c
Jonathan Commins <joncom@gmail.com>
2012-12-21 00:27:01 -0800

    Added example tests with reporting.

--- api/application/controllers/test.php
+++ api/application/controllers/test.php
@@ -12,9 +12,34 @@ class Test extends CI_Controller {
 
         $this->load->library('unit_test');
 
+        $this->unit->set_test_items(array('test_name', 'result'));
+
         $this->unit->use_strict(TRUE);
 
-        echo "Test";
+
+
+        $test = 1 + 1;
+
+        $expected_result = 2;
+
+        $test_name = 'Adds one plus one';
+
+        echo $this->unit->run($test, $expected_result, $test_name);
+
+        $test = 2 + 2;
+
+        $expected_result = 4;
+
+        $test_name = 'Adds two plus two';
+
+        echo $this->unit->run($test, $expected_result, $test_name);
+
+
+
+
+        echo $this->unit->report();
+
+        //print_r( $this->unit->result() );
 
     }
 
