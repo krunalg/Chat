fdb7ac9f3581f3ff077127e3978725d765bb26df
Jonathan Commins <joncom@gmail.com>
2012-12-21 12:42:45 -0800

    Moved user related tests into a single function.

--- api/application/controllers/test.php
+++ api/application/controllers/test.php
@@ -17,92 +17,7 @@ class Test extends CI_Controller {
         $this->unit->use_strict(TRUE);
 
         // Test User_model
-
-        $this->load->model('User_model');
-
-        // Test User->Add/GetByName/GetById/Delete/GetByName
-        {
-
-            $test_name = "Add a user.";
-
-            $expected_result = 201;
-
-            $_POST = array('user' => 'Unlikelyname');
-
-            $test = $this->User_model->add_user();
-
-            $result = $test['code'];
-
-            $this->unit->run($result, $expected_result, $test_name);
-
-            unset($_POST);
-
-        }
-
-        {
-
-            $test_name = "Find a user by name.";
-
-            $expected_result = 1;
-
-            $criteria = array('user' => 'Unlikelyname');
-
-            $test = $this->User_model->get_users( $criteria );
-
-            $result = count( $test['data'] );
-
-            $this->unit->run($result, $expected_result, $test_name);
-
-            // Save user ID for next tests.
-            $user_id = $test['data'][0]->id;
-
-        }
-
-        {
-
-            $test_name = "Find a user by ID.";
-
-            $expected_result = 200;
-
-            $test = $this->User_model->get_user( $user_id );
-
-            $result = $test['code'];
-
-            $this->unit->run($result, $expected_result, $test_name);
-
-        }
-
-        {
-
-            $test_name = "Delete a user by ID.";
-
-            $expected_result = 200;
-
-            $criteria = array('user' => 'Unlikelyname');
-
-            $test = $this->User_model->delete_user( $user_id );
-
-            $result = $test['code'];
-
-            $this->unit->run($result, $expected_result, $test_name);
-
-        }
-
-        {
-
-            $test_name = "Find a user that does not exist.";
-
-            $expected_result = 0;
-
-            $criteria = array('user' => 'Unlikelyname');
-
-            $test = $this->User_model->get_users( $criteria );
-
-            $result = count( $test['data'] );
-
-            $this->unit->run($result, $expected_result, $test_name);
-
-        }
+        $this->_userTest();
 
 
         // Report test results.
@@ -110,6 +25,80 @@ class Test extends CI_Controller {
         echo $this->unit->report();
 
         //print_r( $this->unit->result() );
+
+    }
+
+    private function _userTest() {
+
+        $this->load->model('User_model');
+
+
+        $test_name = "Add a new user to the database.";
+
+        $expected_result = 201;
+
+        $_POST = array('user' => 'Unlikelyname');
+
+        $test = $this->User_model->add_user();
+
+        $result = $test['code'];
+
+        $this->unit->run($result, $expected_result, $test_name);
+
+        unset($_POST);
+
+
+        $test_name = "Fetch newly added user by name.";
+
+        $expected_result = 1;
+
+        $criteria = array('user' => 'Unlikelyname');
+
+        $test = $this->User_model->get_users( $criteria );
+
+        $result = count( $test['data'] );
+
+        $this->unit->run($result, $expected_result, $test_name);
+
+        // Save user ID for next tests.
+        $user_id = $test['data'][0]->id;
+
+
+        $test_name = "Fetch newly added user by obtained ID.";
+
+        $expected_result = 200;
+
+        $test = $this->User_model->get_user( $user_id );
+
+        $result = $test['code'];
+
+        $this->unit->run($result, $expected_result, $test_name);
+
+
+        $test_name = "Delete newly added user by his ID.";
+
+        $expected_result = 200;
+
+        $criteria = array('user' => 'Unlikelyname');
+
+        $test = $this->User_model->delete_user( $user_id );
+
+        $result = $test['code'];
+
+        $this->unit->run($result, $expected_result, $test_name);
+
+
+        $test_name = "Try to find recently deleted user in the database.";
+
+        $expected_result = 0;
+
+        $criteria = array('user' => 'Unlikelyname');
+
+        $test = $this->User_model->get_users( $criteria );
+
+        $result = count( $test['data'] );
+
+        $this->unit->run($result, $expected_result, $test_name);
 
     }
 
