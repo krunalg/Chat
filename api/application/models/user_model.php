183aace082f94dc27a42b6fb226ba386b033364a
Jonathan Commins <joncom@gmail.com>
2012-12-20 16:03:03 -0800

    Removed unused function.

--- api/application/models/user_model.php
+++ api/application/models/user_model.php
@@ -260,38 +260,6 @@ class User_model extends CI_Model {
 
     }
 
-    // Sets HTTP headers and generates a JSON response
-    // which can be used for output.
-    private function _response( $code, $message, $data = NULL, $location = '' ) {
-
-        if( $code == 201 ) {
-
-            // Special case requires URL of newly created resource.
-
-            $this->output->set_header("HTTP/1.1 201 Created");
-
-            $this->output->set_header("Location: $location");
-
-        } else {
-
-            $this->output->set_status_header( $code );
-
-        }
-
-        // If no data is supplied, leave the field out.
-        if( !isset( $data ) ) $response = array( 'code' => $code, 'message' => $message );
-
-        // But if data is supplied, then add it.
-        else $response = array( 'code' => $code, 'message' => $message, 'data' => $data );
-
-        $this->output->set_content_type('application/json');
-
-        $json = json_encode( $response );
-
-        return $json;
-
-    }
-
 }
 
 /* End of file user_model.php */
