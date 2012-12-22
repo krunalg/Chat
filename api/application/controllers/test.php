81454642569475026a854452b6934875b763e470
Jonathan Commins <joncom@gmail.com>
2012-12-21 20:50:36 -0800

    Another working test.

--- api/application/controllers/test.php
+++ api/application/controllers/test.php
@@ -204,22 +204,22 @@ class Test extends CI_Controller {
 
         echo $this->unit->run($result, $expected_result, $test_name, $response->message);
 
-/*
+
         $test_name = "Try adding a new user with predefined ID.";
 
         $path = $controller;
 
+        $post_data = array('username' => 'Legitname', 'id' => 10 );
+
+        $response = $this->apiReponseObj('POST', $path, $post_data );
+
+        $result = $response->code;
+
         $expected_result = 400;
-
-        $_POST = array('username' => 'Legitname', 'id' => 10 );
-
-        $test = $this->User_model->add_user();
-
-        $result = $test['code'];
 
         echo $this->unit->run($result, $expected_result, $test_name, $response->message);
 
-
+/*
         $test_name = "Try adding a new user when there is no POST data.";
 
         $path = $controller;
