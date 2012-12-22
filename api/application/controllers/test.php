57500de9f7edb8f81fc1f7a7570c6bab234d4cd3
Jonathan Commins <joncom@gmail.com>
2012-12-21 23:20:25 -0800

    Added test.

--- api/application/controllers/test.php
+++ api/application/controllers/test.php
@@ -151,6 +151,21 @@ class Test extends CI_Controller {
         echo $this->unit->run($result, $expected_result, $test_name, $response->message);
 
 
+        $test_name = "Try updating newly added user without POST data.";
+
+        $path = $controller . '/' . $user_id;
+
+        $post_data = array();
+
+        $response = $this->apiReponseObj('POST', $path, $post_data );
+
+        $result = $response->code;
+
+        $expected_result = 400;
+
+        echo $this->unit->run($result, $expected_result, $test_name, $response->message);
+
+
         $test_name = "Delete newly added user by his ID.";
 
         $path = $controller . '/' . $user_id;
