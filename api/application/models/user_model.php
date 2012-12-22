27b3f2c25b86c78c9a871b349cba7046b06b2a9b
Jonathan Commins <joncom@gmail.com>
2012-12-21 21:07:07 -0800

    Fixed name of column to validate.

--- api/application/models/user_model.php
+++ api/application/models/user_model.php
@@ -43,7 +43,7 @@ class User_model extends CI_Model {
 
             array(
 
-                'field' => 'facing',
+                'field' => 'direction',
 
                 'label' => 'direction faced',
 
